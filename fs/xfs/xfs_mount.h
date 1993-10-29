48d9fb0045b11e05b1c94ef8de0b970ccf18137e
Doug Doucette <doucette@engr.sgi.com>
1993-10-29 21:17:38 +0000

    Move xfs_mount_t here from xfs.h.

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -1,11 +1,25 @@
 #ifndef _FS_XFS_MOUNT_H
 #define	_FS_XFS_MOUNT_H
 
-#ident	"$Revision: 1.2 $"
+#ident	"$Revision$"
 
-/*
- * xfs_mount struct is in xfs.h for now, move it here later.
- */
+#include "xfs.h"
+#include "xfs_types.h"
+
+typedef struct xfs_mount {
+	xfs_tid_t		m_tid;
+	struct xfs_log_item	*m_ail;
+	uint			m_ail_gen;
+	lock_t			m_ail_lock;
+	xfs_lsn_t		m_ail_lsn;
+	uint			m_log_thresh;
+	struct xfs_trans	*m_async_trans;
+	lock_t			m_async_lock;
+	struct xfs_sb		*m_sb;
+	dev_t			m_dev;
+	int			m_bsize;
+	xfs_agnumber_t		m_agrotor;
+} xfs_mount_t;
 
 void xfs_mod_sb(xfs_trans_t *, int, int);
 void xfs_mount(xfs_mount_t *, dev_t);
