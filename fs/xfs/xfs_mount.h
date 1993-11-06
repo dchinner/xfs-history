03aa7fdda54cf501c4a45a8a308d6ad49a70ac7b
Doug Doucette <doucette@engr.sgi.com>
1993-11-06 22:07:08 +0000

    Get rid of nested includes.  Fix up some fields to use typedef names.

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -3,19 +3,16 @@
 
 #ident	"$Revision$"
 
-#include "xfs.h"
-#include "xfs_types.h"
-
 typedef struct xfs_mount {
 	xfs_tid_t		m_tid;
-	struct xfs_log_item	*m_ail;
+	xfs_log_item_t		*m_ail;
 	uint			m_ail_gen;
 	lock_t			m_ail_lock;
 	xfs_lsn_t		m_ail_lsn;
 	uint			m_log_thresh;
-	struct xfs_trans	*m_async_trans;
+	xfs_trans_t		*m_async_trans;
 	lock_t			m_async_lock;
-	struct xfs_sb		*m_sb;
+	xfs_sb_t		*m_sb;
 	dev_t			m_dev;
 	int			m_bsize;
 	xfs_agnumber_t		m_agrotor;
