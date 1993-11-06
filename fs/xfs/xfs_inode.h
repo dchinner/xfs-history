24f2f0e7118787e63880586514ace04526ad22ae
Doug Doucette <doucette@engr.sgi.com>
1993-11-06 22:05:33 +0000

    Add i_ino, i_bno, i_index fields.  Fixup prototypes to use typedef for xfs_mount_t.

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -2,8 +2,6 @@
 #ifndef	_XFS_INODE_H
 #define	_XFS_INODE_H
 
-#include "xfs_dinode.h"
-
 struct xfs_inode;
 
 typedef struct xfs_inode_log_item {
@@ -21,14 +19,17 @@ typedef struct xfs_inode {
 	xfs_inode_log_item_t	i_item;
 	xfs_trans_t		*i_transp;
 	unsigned int		i_flags;
+	xfs_ino_t		i_ino;		/* inode number (agno/agino) */
+	xfs_agblock_t		i_bno;		/* ag block # of inode */
+	int			i_index;	/* which inode in block */
 	xfs_dinode_t		i_d;
 } xfs_inode_t;
 
 #define	I_LOCKED	0x1
 
 
-xfs_inode_t	*xfs_inode_incore(struct xfs_mount *, xfs_ino_t, void *);
-xfs_inode_t	*xfs_iget(struct xfs_mount *, xfs_ino_t);
+xfs_inode_t	*xfs_inode_incore(xfs_mount_t *, xfs_ino_t, void *);
+xfs_inode_t	*xfs_iget(xfs_mount_t *, xfs_ino_t);
 
 
 #endif	/* _XFS_INODE_H */
