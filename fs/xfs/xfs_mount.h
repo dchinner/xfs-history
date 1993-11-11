2bbe2f2e3b9dd77e03b8f6ede8fb2ffc4c9330a4
Adam Sweeney <ajs@sgi.com>
1993-11-11 19:47:21 +0000

    Add fields for the vfs pointer, the inode hash table, and the active inode list.  Add comments to each field.

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -6,21 +6,27 @@
 struct xfs_ihash;
 
 typedef struct xfs_mount {
-	xfs_tid_t		m_tid;
-	xfs_log_item_t		*m_ail;
-	uint			m_ail_gen;
-	lock_t			m_ail_lock;
-	xfs_lsn_t		m_ail_lsn;
-	uint			m_log_thresh;
-	xfs_trans_t		*m_async_trans;
-	lock_t			m_async_lock;
-	xfs_sb_t		*m_sb;
-	dev_t			m_dev;
-	int			m_bsize;
-	xfs_agnumber_t		m_agrotor;
-	struct xfs_ihash	*m_ihash;
-	ulong			m_ihashmask;
+	struct vfs		*m_vfsp;	/* ptr to vfs */
+	xfs_tid_t		m_tid;		/* next unused tid for fs */
+	xfs_log_item_t		*m_ail;		/* fs active log item list */
+	uint			m_ail_gen;	/* fs AIL generation count */
+	lock_t			m_ail_lock;	/* fs AIL mutex */
+	xfs_lsn_t		m_ail_lsn;	/* lsn of 1st elmt in AIL */
+	uint			m_log_thresh;	/* log head/tail separation */
+	xfs_trans_t		*m_async_trans;	/* list of async transactions */
+	lock_t			m_async_lock;	/* async trans list mutex */
+	xfs_sb_t		*m_sb;		/* ptr to fs superblock */
+	dev_t			m_dev;		/* dev of fs meta-data */
+	int			m_bsize;	/* fs logical block size */
+	xfs_agnumber_t		m_agrotor;	/* last ag where space found */
+	struct xfs_ihash	*m_ihash;	/* fs private inode hash table*/
+	ulong			m_ihashmask;	/* fs inode hash size - 1 */
+	struct xfs_inode	*m_inodes;	/* active inode list */
+	lock_t			m_ilock;	/* inode list mutex */
 } xfs_mount_t;
+
+#define	XFS_MOUNT_ILOCK(mp)	splockspl((mp)->m_ilock, splhi)
+#define	XFS_MOUNT_IUNLOCK(mp,s)	spunlockspl((mp)->m_ilock,(s))
 
 void xfs_mod_sb(xfs_trans_t *, int, int);
 void xfs_mount(xfs_mount_t *, dev_t);
