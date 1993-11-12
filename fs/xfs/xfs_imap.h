ef7e59cadfba7a2a56dccde2f782b8c57f0a2f2b
Doug Doucette <doucette@engr.sgi.com>
1993-11-12 18:52:47 +0000

    Initial revision

---
+++ fs/xfs/xfs_imap.h
@@ -0,0 +1,18 @@
+#ifndef _FS_XFS_IMAP_H
+#define	_FS_XFS_IMAP_H
+
+/*
+ * This is the structure passed to xfs_imap() to map
+ * an inode number to its on disk location.
+ */
+typedef struct xfs_imap {
+	daddr_t		im_blkno;	/* starting BB of inode chunk */
+	uint		im_len;		/* length in BBs of inode chunk */
+	xfs_agblock_t	im_agblkno;	/* logical block of inode chunk in ag */
+	ushort		im_ioffset;	/* inode offset in block in "inodes" */
+	ushort		im_boffset;	/* inode offset in block in bytes */
+} xfs_imap_t;
+	
+int	xfs_imap(xfs_mount_t *, xfs_trans_t *, xfs_ino_t, xfs_imap_t *);
+
+#endif	/* !_FS_XFS_IMAP_H */
