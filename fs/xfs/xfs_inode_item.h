4a634975a1bd48a9ac3ef637c34e3035bafcc900
Adam Sweeney <ajs@sgi.com>
1993-11-25 01:25:13 +0000

    Added inode flushing support.

--- fs/xfs/xfs_inode_item.h
+++ fs/xfs/xfs_inode_item.h
@@ -7,8 +7,10 @@ struct xfs_inode;
 typedef struct xfs_inode_log_item {
 	xfs_log_item_t		ili_item;
 	struct xfs_inode	*ili_inode;
+	xfs_lsn_t		ili_flush_lsn;
 	unsigned short		ili_recur;
 	unsigned short		ili_flags;
+	unsigned short		ili_ref;
 	unsigned int		ili_fields;
 } xfs_inode_log_item_t;
 
@@ -17,11 +19,14 @@ typedef struct xfs_inode_log_item {
 /*
  * Flags for xfs_trans_log_inode flags field.
  */
-#define	XFS_ILOG_META	0x001	/* log standard inode fields */
+#define	XFS_ILOG_CORE	0x001	/* log standard inode fields */
 #define	XFS_ILOG_DATA	0x002	/* log iu_data */
 #define	XFS_ILOG_EXT	0x004	/* log iu_extents */
 #define	XFS_ILOG_BROOT	0x008	/* log i_broot */
 #define	XFS_ILOG_DEV	0x010	/* log the dev field */
+
+#define	XFS_ILOG_NONCORE	(XFS_ILOG_DATA | XFS_ILOG_EXT | \
+				 XFS_ILOG_BROOT | XFS_ILOG_DEV)
 
 /*
  * This is the structure used to lay out an inode log item in the
@@ -43,5 +48,6 @@ typedef struct xfs_inode_log_format {
 } xfs_inode_log_format_t;
 	
 extern void	xfs_inode_item_init(struct xfs_inode *, xfs_mount_t *);
+extern void	xfs_iflush_done(buf_t *, xfs_inode_log_item_t *);
 
 #endif	/* _XFS_INODE_ITEM_H */
