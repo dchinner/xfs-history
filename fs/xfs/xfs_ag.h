003790aada3a5c86d3f5c07d4ee874e7301cc4f6
Doug Doucette <doucette@engr.sgi.com>
1993-11-29 21:26:43 +0000

    Get rid of inode btree.  Change XFS_AG_MIN and MAX macros to depend on the block size.  New inode mapping scheme.

--- fs/xfs/xfs_ag.h
+++ fs/xfs/xfs_ag.h
@@ -12,8 +12,7 @@
 
 typedef enum
 {
-	XFS_BTNUM_BNO, XFS_BTNUM_CNT, XFS_BTNUM_IBT, XFS_BTNUM_BMAP,
-	XFS_BTNUM_MAX
+	XFS_BTNUM_BNO, XFS_BTNUM_CNT, XFS_BTNUM_BMAP, XFS_BTNUM_MAX
 } xfs_btnum_t;
 
 typedef struct xfs_aghdr
@@ -25,20 +24,20 @@ typedef struct xfs_aghdr
 	/*
 	 * Freespace information
 	 */
-	xfs_agblock_t	ag_roots[XFS_BTNUM_MAX - 2];
+	xfs_agblock_t	ag_roots[XFS_BTNUM_MAX - 1];
 	xfs_agblock_t	ag_freelist;	/* free blocks */
-	__uint16_t	ag_levels[XFS_BTNUM_MAX - 2];
+	__uint16_t	ag_levels[XFS_BTNUM_MAX - 1];
 	xfs_extlen_t	ag_flist_count;	/* #blocks */
 	xfs_extlen_t	ag_freeblks;	/* total free blocks */
 	xfs_extlen_t	ag_longest;	/* longest free space */
 	/*
-	 * We may really want to put the inode btree root here instead
-	 * of pointing at it, but for now it simplifies things to not
-	 * have to special case the root block of the Btree.
+	 * Inode information
+	 * Inodes are mapped by interpreting the inode number, so no
+	 * mapping data is needed here.
 	 */
-	xfs_agblock_t	ag_iroot;	/* root block of B-tree for inode map */
 	xfs_agino_t	ag_icount;	/* count of allocated inodes */
-	__uint16_t	ag_ilevels;	/* # levels in the B-tree */
+	xfs_agino_t	ag_ifirst;	/* first allocated inode */
+	xfs_agino_t	ag_ilast;	/* last allocated inode */
 	xfs_agino_t	ag_iflist;	/* first free inode */
 	xfs_agino_t	ag_ifcount;	/* number of free inodes */
 } xfs_aghdr_t;
@@ -53,9 +52,9 @@ typedef struct xfs_aghdr
 #define	XFS_AG_FLIST_COUNT	0x0080
 #define	XFS_AG_FREEBLKS		0x0100
 #define	XFS_AG_LONGEST		0x0200
-#define	XFS_AG_IROOT		0x0400
-#define	XFS_AG_ICOUNT		0x0800
-#define	XFS_AG_ILEVELS		0x1000
+#define	XFS_AG_ICOUNT		0x0400
+#define	XFS_AG_IFIRST		0x0800
+#define	XFS_AG_ILAST		0x1000
 #define	XFS_AG_IFLIST		0x2000
 #define	XFS_AG_IFCOUNT		0x4000
 #define	XFS_AG_NUM_BITS		15
@@ -64,8 +63,11 @@ typedef struct xfs_aghdr
 /* block number in the AG; SB is block 0 */
 #define	XFS_AGH_BLOCK	((xfs_agblock_t)(XFS_SB_BLOCK + 1))
 
-#define	XFS_AG_MIN_SIZE	(1<<24)	/* bytes */
-#define	XFS_AG_MAX_SIZE	(1<<30)	/* bytes */
+#define	XFS_AG_MIN_BYTES	(1LL << 24)	/* 16 MB */
+#define	XFS_AG_MAX_BYTES	(1LL << 32)	/*  4 GB */
+
+#define	XFS_AG_MIN_BLOCKS(bl)	((xfs_extlen_t)(XFS_AG_MIN_BYTES >> bl))
+#define	XFS_AG_MAX_BLOCKS(bl)	((xfs_extlen_t)(XFS_AG_MAX_BYTES >> bl))
 
 #define	XFS_MIN_FREELIST(a)	(2 * (((a)->ag_levels[XFS_BTNUM_BNO]) + ((a)->ag_levels[XFS_BTNUM_CNT]) + 1))
 
