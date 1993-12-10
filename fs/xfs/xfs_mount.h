59cd5f994d50023756bda7f8f2396a9ea4ea6edf
Adam Sweeney <ajs@sgi.com>
1993-12-10 00:22:04 +0000

    Add more prototypes.

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -16,6 +16,8 @@ typedef struct xfs_mount {
 	xfs_trans_t		*m_async_trans;	/* list of async transactions */
 	lock_t			m_async_lock;	/* async trans list mutex */
 	xfs_sb_t		m_sb;		/* ptr to fs superblock */
+	lock_t			m_sb_lock;	/* sb counter mutex */
+	buf_t			*m_sb_bp;	/* buffer for superblock */
 	dev_t			m_dev;		/* dev of fs meta-data */
 	int			m_bsize;	/* fs logical block size */
 	xfs_agnumber_t		m_agrotor;	/* last ag where space found */
@@ -26,12 +28,26 @@ typedef struct xfs_mount {
 	lock_t			m_ilock;	/* inode list mutex */
 	void			*m_log;		/* log specific stuff */
 } xfs_mount_t;
+ 
+/*
+ * This structure is for use by the xfs_mod_incore_sb_batch() routine.
+ *
+ */
+typedef struct xfs_mod_sb {
+	uint	msb_field;	/* Field to modify, see below */
+	int	msb_delta;	/* change to make to the specified field */
+} xfs_mod_sb_t;
 
 #define	XFS_MOUNT_ILOCK(mp)	splockspl((mp)->m_ilock, splhi)
 #define	XFS_MOUNT_IUNLOCK(mp,s)	spunlockspl((mp)->m_ilock,(s))
+#define	XFS_SB_LOCK(mp)		splockspl((mp)->m_sb_lock, splhi)
+#define	XFS_SB_UNLOCK(mp,s)	spunlockspl((mp)->m_sb_lock,(s))
 
-void xfs_mod_sb(xfs_trans_t *, int);
-void xfs_mount(xfs_mount_t *, dev_t);
-void xfs_umount(xfs_mount_t *);
+void	xfs_mod_sb(xfs_trans_t *, int);
+void	xfs_mount(xfs_mount_t *, dev_t);
+void	xfs_umount(xfs_mount_t *);
+int	xfs_mod_incore_sb(xfs_mount_t *, uint, int);
+int	xfs_mod_incore_sb_batch(xfs_mount_t *, xfs_mod_sb_t *, uint);
+buf_t	*xfs_getsb(xfs_mount_t *);
 
 #endif	/* !_FS_XFS_MOUNT_H */
