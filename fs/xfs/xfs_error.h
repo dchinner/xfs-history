63d9d6b4701ee47c62cf810ab9a2747673f13bdd
Doug Doucette <doucette@engr.sgi.com>
1993-12-15 01:12:20 +0000

    Initial revision

---
+++ fs/xfs/xfs_error.h
@@ -0,0 +1,9 @@
+#ifndef	_XFS_ERROR_H
+#define	_XFS_ERROR_H
+
+#define XFS_ERECOVER	1	/* Failure to recover log */
+#define XFS_ELOGSTAT	2	/* Failure to stat log in user space */
+#define XFS_ENOLOGSPACE	3	/* Reservation too large */
+#define XFS_ENOTSUP	4
+
+#endif	/* _XFS_ERROR_H */
