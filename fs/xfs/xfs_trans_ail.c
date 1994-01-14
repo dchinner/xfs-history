11b13afc05daf7d2ec9206f88a2f97add3abbc9c
Adam Sweeney <ajs@sgi.com>
1994-01-14 20:06:48 +0000

    Implement the AIL for real.  It's a doubly linked list.

--- fs/xfs/xfs_trans_ail.c
+++ fs/xfs/xfs_trans_ail.c
@@ -20,10 +20,16 @@
 #include "sim.h"
 #endif
 
-STATIC void		sbb_insert(xfs_log_item_t **, xfs_log_item_t*);
-STATIC xfs_log_item_t	*sbb_delete(xfs_log_item_t **, xfs_log_item_t*);
-STATIC xfs_log_item_t	*sbb_min(xfs_log_item_t **);
-STATIC xfs_log_item_t	*sbb_next(xfs_log_item_t **, xfs_log_item_t*);
+STATIC void	xfs_ail_insert(xfs_ail_entry_t *, xfs_log_item_t *);
+STATIC xfs_log_item_t	*xfs_ail_delete(xfs_ail_entry_t	*, xfs_log_item_t *);
+STATIC xfs_log_item_t	*xfs_ail_min(xfs_ail_entry_t *);
+STATIC xfs_log_item_t	*xfs_ail_next(xfs_ail_entry_t *, xfs_log_item_t	*);
+
+#ifdef XFSDEBUG
+STATIC void	xfs_ail_check(xfs_ail_entry_t *);
+#else
+#define	xfs_ail_check(a)
+#endif /* XFSDEBUG */
 
 /*
  * This will be called from xfs_trans_do_commit() after a transaction
@@ -128,22 +134,24 @@ xfs_trans_push_ail(struct xfs_mount *mp)
  * has changed.
  */
 void
-xfs_trans_update_ail(struct xfs_mount *mp, xfs_log_item_t *lip, xfs_lsn_t lsn)
+xfs_trans_update_ail(xfs_mount_t	*mp,
+		     xfs_log_item_t	*lip,
+		     xfs_lsn_t		lsn)
 {
-	xfs_log_item_t		**ailp;
+	xfs_ail_entry_t		*ailp;
 	xfs_log_item_t		*dlip;
 
 	ailp = &(mp->m_ail);
 	if (lip->li_flags & XFS_LI_IN_AIL) {
-		dlip = sbb_delete(ailp, lip);
-		/* ASSERT(dlip == lip); */
+		dlip = xfs_ail_delete(ailp, lip);
+		ASSERT(dlip == lip);
 	} else {
 		lip->li_flags |= XFS_LI_IN_AIL;
 	}
 
 	lip->li_lsn = lsn;
 
-	sbb_insert(ailp, lip);
+	xfs_ail_insert(ailp, lip);
 	mp->m_ail_gen++;
 }
 
@@ -156,16 +164,17 @@ xfs_trans_update_ail(struct xfs_mount *mp, xfs_log_item_t *lip, xfs_lsn_t lsn)
  * has changed.
  */
 void
-xfs_trans_delete_ail(struct xfs_mount *mp, xfs_log_item_t *lip)
+xfs_trans_delete_ail(xfs_mount_t	*mp,
+		     xfs_log_item_t	*lip)
 {
-	xfs_log_item_t		**ailp;
+	xfs_ail_entry_t		*ailp;
 	xfs_log_item_t		*dlip;
 
-	/* ASSERT(lip->li_flags & XFS_LI_IN_AIL); */
+	ASSERT(lip->li_flags & XFS_LI_IN_AIL);
 
 	ailp = &(mp->m_ail);
-	dlip = sbb_delete(ailp, lip);
-	/* ASSERT(dlip == lip); */
+	dlip = xfs_ail_delete(ailp, lip);
+	ASSERT(dlip == lip);
 
 	lip->li_flags &= ~XFS_LI_IN_AIL;
 	lip->li_lsn = 0;
@@ -181,11 +190,12 @@ xfs_trans_delete_ail(struct xfs_mount *mp, xfs_log_item_t *lip)
  * in calls to xfs_trans_next_ail().
  */
 xfs_log_item_t *
-xfs_trans_first_ail(struct xfs_mount *mp, int *gen)
+xfs_trans_first_ail(xfs_mount_t	*mp,
+		    int		*gen)
 {
 	xfs_log_item_t	*lip;
 
-	lip = sbb_min(&(mp->m_ail));
+	lip = xfs_ail_min(&(mp->m_ail));
 	*gen = (int)mp->m_ail_gen;
 
 	return (lip);
@@ -198,57 +208,172 @@ xfs_trans_first_ail(struct xfs_mount *mp, int *gen)
  * then return the minimum elmt of the AIL.
  */
 xfs_log_item_t *
-xfs_trans_next_ail(struct xfs_mount *mp, xfs_log_item_t *lip, int *gen)
+xfs_trans_next_ail(xfs_mount_t		*mp,
+		   xfs_log_item_t	*lip,
+		   int			*gen)
 {
 	xfs_log_item_t	*nlip;
 
 	if (mp->m_ail_gen == *gen) {
-		nlip = sbb_next(&(mp->m_ail), lip);
+		nlip = xfs_ail_next(&(mp->m_ail), lip);
 	} else {
-		nlip = sbb_min(&(mp->m_ail));
+		nlip = xfs_ail_min(&(mp->m_ail));
 	}
 
 	return (nlip);
 }
 
 
+/*
+ * The active item list (AIL) is a doubly linked list of log
+ * items sorted by ascending lsn.  The base of the list is
+ * a forw/back pointer pair embedded in the xfs mount structure.
+ * The base is initialized with both pointers pointing to the
+ * base.  This case always needs to be distinguished, because
+ * the base has no lsn to look at.  We almost always insert
+ * at the end of the list, so on inserts we search from the
+ * end of the list to find where the new item belongs.
+ */
 
+/*
+ * Initialize the doubly linked list to point only to itself.
+ */
+void
+xfs_trans_ail_init(xfs_mount_t *mp)
+{
+	mp->m_ail.ail_forw = (xfs_log_item_t*)&(mp->m_ail);
+	mp->m_ail.ail_back = (xfs_log_item_t*)&(mp->m_ail);
+}
 
-
-
+/*
+ * Insert the given log item into the AIL.
+ * We almost always insert at the end of the list, so on inserts
+ * we search from the end of the list to find where the
+ * new item belongs.
+ */
 STATIC void
-sbb_insert(xfs_log_item_t **root, xfs_log_item_t *lip)
+xfs_ail_insert(xfs_ail_entry_t	*base,
+	       xfs_log_item_t	*lip)
 /* ARGSUSED */
 {
+	xfs_log_item_t	*next_lip;
+
+	/*
+	 * If the list is empty, just insert the item.
+	 */
+	if (base->ail_back == (xfs_log_item_t*)base) {
+		base->ail_forw = lip;
+		base->ail_back = lip;
+		lip->li_ail.ail_forw = (xfs_log_item_t*)base;
+		lip->li_ail.ail_back = (xfs_log_item_t*)base;
+		return;
+	}
+
+	next_lip = base->ail_back;
+	while ((next_lip != (xfs_log_item_t*)base) &&
+	       (next_lip->li_lsn > lip->li_lsn)) {
+		next_lip = next_lip->li_ail.ail_back;
+	}
+	ASSERT((next_lip == (xfs_log_item_t*)base) ||
+	       (next_lip->li_lsn <= lip->li_lsn));
+	lip->li_ail.ail_forw = next_lip->li_ail.ail_forw;
+	lip->li_ail.ail_back = next_lip;
+	next_lip->li_ail.ail_forw = lip;
+	lip->li_ail.ail_forw->li_ail.ail_back = lip;
+
+	xfs_ail_check(base);
 	return;
 }
 
-
+/*
+ * Delete the given item from the AIL.  Return a pointer to the item.
+ */
 STATIC xfs_log_item_t *
-sbb_delete(xfs_log_item_t **root, xfs_log_item_t *lip)
+xfs_ail_delete(xfs_ail_entry_t	*base,
+	       xfs_log_item_t	*lip)
 /* ARGSUSED */
 {
-	return (NULL);
+	lip->li_ail.ail_forw->li_ail.ail_back = lip->li_ail.ail_back;
+	lip->li_ail.ail_back->li_ail.ail_forw = lip->li_ail.ail_forw;
+	lip->li_ail.ail_forw = NULL;
+	lip->li_ail.ail_back = NULL;
+
+	xfs_ail_check(base);
+	return lip;
 }
 
-
+/*
+ * Return a pointer to the first item in the AIL.
+ * If the AIL is empty, then return NULL.
+ */
 STATIC xfs_log_item_t *
-sbb_min(xfs_log_item_t **root)
+xfs_ail_min(xfs_ail_entry_t *base)
 /* ARGSUSED */
 {
-	return (NULL);
+	if (base->ail_forw == (xfs_log_item_t*)base) {
+		return NULL;
+	}
+	return base->ail_forw;
 
 }
 
-
+/*
+ * Return a pointer to the item which follows
+ * the given item in the AIL.  If the given item
+ * is the last item in the list, then return NULL.
+ */
 STATIC xfs_log_item_t *
-sbb_next(xfs_log_item_t **root, xfs_log_item_t *lip)
+xfs_ail_next(xfs_ail_entry_t	*base,
+	     xfs_log_item_t	*lip)
 /* ARGSUSED */
 {
-	return (NULL);
+	if (lip->li_ail.ail_forw == (xfs_log_item_t*)base) {
+		return NULL;
+	}
+	return lip->li_ail.ail_forw;
 
 }
 
+#ifdef XFSDEBUG
+/*
+ * Check that the list is sorted as it should be.
+ */
+STATIC void
+xfs_ail_check(xfs_ail_entry_t *base)
+{
+	xfs_log_item_t	*lip;
+	xfs_log_item_t	*prev_lip;
+
+	lip = base->ail_forw;
+	if (lip == (xfs_log_item_t*)base) {
+		/*
+		 * Make sure the pointers are correct when the list
+		 * is empty.
+		 */
+		ASSERT(base->ail_back == (xfs_log_item_t*)base);
+		return;
+	}
+
+	/*
+	 * Walk the list checking forward and backward pointers,
+	 * lsn ordering, and that every entry has the XFS_LI_IN_AIL
+	 * flag set.
+	 */
+	prev_lip = (xfs_log_item_t*)base;
+	while (lip != (xfs_log_item_t*)base) {
+		if (prev_lip != (xfs_log_item_t*)base) {
+			ASSERT(prev_lip->li_ail.ail_forw == lip);
+			ASSERT(prev_lip->li_lsn <= lip->li_lsn);
+		}
+		ASSERT(lip->li_ail.ail_back == prev_lip);
+		ASSERT((lip->li_flags & XFS_LI_IN_AIL) != 0);
+		prev_lip = lip;
+		lip = lip->li_ail.ail_forw;
+	}
+	ASSERT(lip == (xfs_log_item_t*)base);
+	ASSERT(base->ail_back == prev_lip);
+}
+#endif /* XFSDEBUG */
 
 
 
