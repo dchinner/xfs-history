ef1091d7991f29c2714b8f88a16b45650789ad38
Doug Doucette <doucette@engr.sgi.com>
1994-02-08 19:52:27 +0000

    Add more realtime fields to the superblock.  Fix up all the bit values.

--- fs/xfs/xfs_sb.h
+++ fs/xfs/xfs_sb.h
@@ -18,17 +18,19 @@ typedef struct xfs_sb
 	xfs_fsblock_t	sb_dblocks;	/* number of data blocks */
 	__uint32_t	sb_blocksize;	/* logical block size, bytes */
 	/*
-	 * sb_magic is at offset 28 to be at the same location as fs_magic
+	 * sb_magicnum is at offset 28 to be at the same location as fs_magic
 	 * in an EFS filesystem, thus ensuring there is no confusion.
 	 */
 	__uint32_t	sb_magicnum;	/* magic number == XFS_SB_MAGIC */
 	xfs_fsblock_t	sb_rblocks;	/* number of realtime blocks */
-	xfs_fsblock_t	sb_rbitmap;	/* bitmap for realtime blocks */
-	xfs_fsblock_t	sb_rsummary;	/* summary for xfs_rbitmap */
+	xfs_fsblock_t	sb_rextents;	/* number of realtime extents */
 	xfs_ino_t	sb_rootino;	/* root inode number */
+	xfs_ino_t	sb_rbmino;	/* bitmap inode for realtime extents */
+	xfs_ino_t	sb_rsumino;	/* summary inode for rt bitmap */
 	xfs_agblock_t	sb_rextsize;	/* realtime extent size, blocks */
 	xfs_agblock_t	sb_agblocks;	/* size of an allocation group */
 	xfs_agnumber_t	sb_agcount;	/* number of allocation groups */
+	xfs_extlen_t	sb_rbmblocks;	/* number of rt bitmap blocks */
 	__uint16_t	sb_versionnum;	/* header version == XFS_SB_VERSION */
 	__uint16_t	sb_sectsize;	/* volume sector size, bytes */
 	__uint16_t	sb_inodesize;	/* inode size, bytes */
@@ -40,6 +42,7 @@ typedef struct xfs_sb
 	__uint8_t	sb_inodelog;	/* log2 of sb_inodesize */
 	__uint8_t	sb_inopblog;	/* log2 of sb_inopblock */
 	__uint8_t	sb_agblklog;	/* log2 of sb_agblocks (rounded up) */
+	__uint8_t	sb_rextslog;	/* log2 of sb_rextents */
 					/* other inode config information? */
 					/* statistics */
 	/*
@@ -50,37 +53,40 @@ typedef struct xfs_sb
 	__uint64_t	sb_icount;	/* allocated inodes */
 	__uint64_t	sb_ifree;	/* free inodes */
 	__uint64_t	sb_fdblocks;	/* free data blocks */
-	__uint32_t	sb_frextents;	/* free realtime extents */
+	__uint64_t	sb_frextents;	/* free realtime extents */
 } xfs_sb_t;
 
 
-#define	XFS_SB_UUID		0x0000001
-#define	XFS_SB_DBLOCKS		0x0000002
-#define	XFS_SB_BLOCKSIZE	0x0000004
-#define	XFS_SB_MAGICNUM		0x0000008
-#define	XFS_SB_RBLOCKS		0x0000010
-#define	XFS_SB_RBITMAP		0x0000020
-#define	XFS_SB_RSUMMARY		0x0000040
-#define	XFS_SB_ROOTINO		0x0000080
-#define	XFS_SB_REXTSIZE		0x0000100
-#define	XFS_SB_AGBLOCKS		0x0000200
-#define	XFS_SB_AGCOUNT		0x0000400
-#define	XFS_SB_VERSIONNUM	0x0000800
-#define	XFS_SB_SECTSIZE		0x0001000
-#define	XFS_SB_INODESIZE	0x0002000
-#define	XFS_SB_INOPBLOCK	0x0004000
-#define	XFS_SB_FNAME		0x0008000
-#define	XFS_SB_FPACK		0x0010000
-#define	XFS_SB_BLOCKLOG		0x0020000
-#define	XFS_SB_SECTLOG		0x0040000
-#define	XFS_SB_INODELOG		0x0080000
-#define	XFS_SB_INOPBLOG		0x0100000
-#define	XFS_SB_AGBLKLOG		0x0200000
-#define	XFS_SB_ICOUNT		0x0400000
-#define	XFS_SB_IFREE		0x0800000
-#define	XFS_SB_FDBLOCKS		0x1000000
-#define	XFS_SB_FREXTENTS	0x2000000
-#define	XFS_SB_NUM_BITS		26
+#define	XFS_SB_UUID		0x00000001
+#define	XFS_SB_DBLOCKS		0x00000002
+#define	XFS_SB_BLOCKSIZE	0x00000004
+#define	XFS_SB_MAGICNUM		0x00000008
+#define	XFS_SB_RBLOCKS		0x00000010
+#define	XFS_SB_REXTENTS		0x00000020
+#define	XFS_SB_ROOTINO		0x00000040
+#define	XFS_SB_RBMINO		0x00000080
+#define	XFS_SB_RSUMINO		0x00000100
+#define	XFS_SB_REXTSIZE		0x00000200
+#define	XFS_SB_AGBLOCKS		0x00000400
+#define	XFS_SB_AGCOUNT		0x00000800
+#define	XFS_SB_RBMBLOCKS	0x00001000
+#define	XFS_SB_VERSIONNUM	0x00002000
+#define	XFS_SB_SECTSIZE		0x00004000
+#define	XFS_SB_INODESIZE	0x00008000
+#define	XFS_SB_INOPBLOCK	0x00010000
+#define	XFS_SB_FNAME		0x00020000
+#define	XFS_SB_FPACK		0x00040000
+#define	XFS_SB_BLOCKLOG		0x00080000
+#define	XFS_SB_SECTLOG		0x00100000
+#define	XFS_SB_INODELOG		0x00200000
+#define	XFS_SB_INOPBLOG		0x00400000
+#define	XFS_SB_AGBLKLOG		0x00800000
+#define	XFS_SB_REXTSLOG		0x01000000
+#define	XFS_SB_ICOUNT		0x02000000
+#define	XFS_SB_IFREE		0x04000000
+#define	XFS_SB_FDBLOCKS		0x08000000
+#define	XFS_SB_FREXTENTS	0x10000000
+#define	XFS_SB_NUM_BITS		29
 #define	XFS_SB_ALL_BITS		((1 << XFS_SB_NUM_BITS) - 1)
 
 #define	XFS_SB_DADDR	((daddr_t)0)		/* daddr in filesystem/ag */
