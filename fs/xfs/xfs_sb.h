699377f803ded2094a543ec8d486e8be5e331a99
Doug Doucette <doucette@engr.sgi.com>
1994-02-15 00:23:52 +0000

    Add superblock fields for internal logs.

--- fs/xfs/xfs_sb.h
+++ fs/xfs/xfs_sb.h
@@ -24,6 +24,7 @@ typedef struct xfs_sb
 	__uint32_t	sb_magicnum;	/* magic number == XFS_SB_MAGIC */
 	xfs_fsblock_t	sb_rblocks;	/* number of realtime blocks */
 	xfs_fsblock_t	sb_rextents;	/* number of realtime extents */
+	xfs_fsblock_t	sb_logstart;	/* starting block of log if internal */
 	xfs_ino_t	sb_rootino;	/* root inode number */
 	xfs_ino_t	sb_rbmino;	/* bitmap inode for realtime extents */
 	xfs_ino_t	sb_rsumino;	/* summary inode for rt bitmap */
@@ -31,6 +32,7 @@ typedef struct xfs_sb
 	xfs_agblock_t	sb_agblocks;	/* size of an allocation group */
 	xfs_agnumber_t	sb_agcount;	/* number of allocation groups */
 	xfs_extlen_t	sb_rbmblocks;	/* number of rt bitmap blocks */
+	xfs_extlen_t	sb_logblocks;	/* number of log blocks */
 	__uint16_t	sb_versionnum;	/* header version == XFS_SB_VERSION */
 	__uint16_t	sb_sectsize;	/* volume sector size, bytes */
 	__uint16_t	sb_inodesize;	/* inode size, bytes */
@@ -63,30 +65,32 @@ typedef struct xfs_sb
 #define	XFS_SB_MAGICNUM		0x00000008
 #define	XFS_SB_RBLOCKS		0x00000010
 #define	XFS_SB_REXTENTS		0x00000020
-#define	XFS_SB_ROOTINO		0x00000040
-#define	XFS_SB_RBMINO		0x00000080
-#define	XFS_SB_RSUMINO		0x00000100
-#define	XFS_SB_REXTSIZE		0x00000200
-#define	XFS_SB_AGBLOCKS		0x00000400
-#define	XFS_SB_AGCOUNT		0x00000800
-#define	XFS_SB_RBMBLOCKS	0x00001000
-#define	XFS_SB_VERSIONNUM	0x00002000
-#define	XFS_SB_SECTSIZE		0x00004000
-#define	XFS_SB_INODESIZE	0x00008000
-#define	XFS_SB_INOPBLOCK	0x00010000
-#define	XFS_SB_FNAME		0x00020000
-#define	XFS_SB_FPACK		0x00040000
-#define	XFS_SB_BLOCKLOG		0x00080000
-#define	XFS_SB_SECTLOG		0x00100000
-#define	XFS_SB_INODELOG		0x00200000
-#define	XFS_SB_INOPBLOG		0x00400000
-#define	XFS_SB_AGBLKLOG		0x00800000
-#define	XFS_SB_REXTSLOG		0x01000000
-#define	XFS_SB_ICOUNT		0x02000000
-#define	XFS_SB_IFREE		0x04000000
-#define	XFS_SB_FDBLOCKS		0x08000000
-#define	XFS_SB_FREXTENTS	0x10000000
-#define	XFS_SB_NUM_BITS		29
+#define	XFS_SB_LOGSTART		0x00000040
+#define	XFS_SB_ROOTINO		0x00000080
+#define	XFS_SB_RBMINO		0x00000100
+#define	XFS_SB_RSUMINO		0x00000200
+#define	XFS_SB_REXTSIZE		0x00000400
+#define	XFS_SB_AGBLOCKS		0x00000800
+#define	XFS_SB_AGCOUNT		0x00001000
+#define	XFS_SB_RBMBLOCKS	0x00002000
+#define	XFS_SB_LOGBLOCKS	0x00004000
+#define	XFS_SB_VERSIONNUM	0x00008000
+#define	XFS_SB_SECTSIZE		0x00010000
+#define	XFS_SB_INODESIZE	0x00020000
+#define	XFS_SB_INOPBLOCK	0x00040000
+#define	XFS_SB_FNAME		0x00080000
+#define	XFS_SB_FPACK		0x00100000
+#define	XFS_SB_BLOCKLOG		0x00200000
+#define	XFS_SB_SECTLOG		0x00400000
+#define	XFS_SB_INODELOG		0x00800000
+#define	XFS_SB_INOPBLOG		0x01000000
+#define	XFS_SB_AGBLKLOG		0x02000000
+#define	XFS_SB_REXTSLOG		0x04000000
+#define	XFS_SB_ICOUNT		0x08000000
+#define	XFS_SB_IFREE		0x10000000
+#define	XFS_SB_FDBLOCKS		0x20000000
+#define	XFS_SB_FREXTENTS	0x40000000
+#define	XFS_SB_NUM_BITS		31
 #define	XFS_SB_ALL_BITS		((1 << XFS_SB_NUM_BITS) - 1)
 
 #define	XFS_SB_DADDR	((daddr_t)0)		/* daddr in filesystem/ag */
