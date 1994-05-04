96d42011e02d09681eb785ccafe454d8dba2a97c
Doug Doucette <doucette@engr.sgi.com>
1994-05-04 23:18:29 +0000

    Initial revision

---
+++ fs/xfs/xfs_bit.h
@@ -0,0 +1,45 @@
+#ifndef _FS_XFS_BIT_H
+#define	_FS_XFS_BIT_H
+
+#ident "$Revision$"
+
+/*
+ * xFS bit manipulation routines.
+ */
+
+/*
+ * Index of low bit number in byte, -1 for none set, 0..7 otherwise.
+ */
+extern const char xfs_lowbit[256];
+
+/*
+ * Index of high bit number in byte, -1 for none set, 0..7 otherwise.
+ */
+extern const char xfs_highbit[256];
+
+/*
+ * Count of bits set in byte, 0..8.
+ */
+extern const char xfs_countbit[256];
+
+/*
+ * xfs_lowbit32: get low bit set out of 32-bit argument, -1 if none set.
+ */
+extern int xfs_lowbit32(__uint32_t v);
+
+/*
+ * xfs_highbit32: get high bit set out of 32-bit argument, -1 if none set.
+ */
+extern int xfs_highbit32(__uint32_t v);
+
+/*
+ * xfs_lowbit64: get low bit set out of 64-bit argument, -1 if none set.
+ */
+extern int xfs_lowbit64(__uint64_t v);
+
+/*
+ * xfs_highbit64: get high bit set out of 64-bit argument, -1 if none set.
+ */
+extern int xfs_highbit64(__uint64_t);
+
+#endif	/* _FS_XFS_BIT_H */
