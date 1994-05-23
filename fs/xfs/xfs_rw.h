0b58ea32d97e243e11ed5ccd6981c6d84465ca48
Adam Sweeney <ajs@sgi.com>
1994-05-23 21:51:44 +0000

    Define structure to hold xfs_strat_write() local variables.

--- fs/xfs/xfs_rw.h
+++ fs/xfs/xfs_rw.h
@@ -5,6 +5,44 @@
 
 
 /*
+ * This is a structure used to hold the local variables used
+ * in xfs_strat_write().  We dynamically allocate this to reduce
+ * the amount of stack space we use.  This is here in the public
+ * header file since it is exported to xfs_init() so it can
+ * initialize the zone allocator.
+ */
+typedef struct xfs_strat_write_locals {
+	xfs_fileoff_t	offset_fsb;
+	xfs_fileoff_t   map_start_fsb;
+	xfs_fileoff_t	imap_offset;
+	xfs_fsblock_t	first_block;
+	xfs_extlen_t	count_fsb;
+	xfs_extlen_t	imap_blocks;
+	int		x;
+	caddr_t		datap;
+	buf_t		*rbp;
+	xfs_mount_t	*mp;
+	xfs_inode_t	*ip;
+	xfs_trans_t	*tp;
+	int		error;
+	xfs_bmap_free_t	free_list;
+	xfs_bmbt_irec_t	*imapp;
+	int		rbp_offset;
+	int		rbp_len;
+	int		set_lead;
+	int		s;
+	int		imap_index;
+	int		nimaps;
+	xfs_bmbt_irec_t	imap[XFS_BMAP_MAX_NMAP];
+} xfs_strat_write_locals_t;
+
+/*
+ * Count of bmaps allocated in one call to the xfs_bmap_zone
+ * allocator.
+ */
+#define	XFS_ZONE_NBMAPS	4
+
+/*
  * Prototypes for functions in xfs_rw.c.
  */
 int
