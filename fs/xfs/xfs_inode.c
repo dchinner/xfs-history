17b376d15af23d73ac76a3b246e8d1b5e0ba15b0
Adam Sweeney <ajs@sgi.com>
1994-07-06 20:18:54 +0000

    Reduce the maximum number of extents that we will free from a file in a single transaction significantly.  Freeing an extent can touch a large number of allocation btree blocks, so we need to limit the number we'll need to lock at once.

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -54,9 +54,10 @@
 zone_t *xfs_inode_zone;
 
 /*
- * Used in xfs_itruncate().  This number needs more thought.
+ * Used in xfs_itruncate().  This is the maximum number of extents
+ * freed from a file in a single transaction.
  */
-#define	XFS_ITRUNC_MAX_EXTENTS	128
+#define	XFS_ITRUNC_MAX_EXTENTS	4
 
 STATIC buf_t *
 xfs_inotobp(
