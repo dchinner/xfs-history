041fe9e4ff43d06d071d5160914fadc2e155580a
Tom Phelan <tap@sgi.com>
1994-09-27 00:14:46 +0000

    allow the setting of file extent size only if the file is marked as real time.

--- fs/xfs/xfs_vnodeops.c
+++ fs/xfs/xfs_vnodeops.c
@@ -617,6 +617,24 @@ xfs_setattr(vnode_t	*vp,
 			code = XFS_ERROR(EINVAL);	/* EFBIG? */
 			goto error_return;
 		}
+
+		/*
+		 * Can't set extent size unless the file is marked, or
+		 * about to be marked as a realtime file.	
+		 *
+		 * This check will be removed when fixed size extents
+		 * with buffered data writes is implemented.
+		 *
+		 */
+		if ( (mask & AT_EXTSIZE) 			&&
+		     (ip->i_d.di_extsize != vap->va_extsize)	&&
+		     ( !( (ip->i_d.di_flags & XFS_DIFLAG_REALTIME)  ||
+		          ((mask & AT_XFLAGS) &&
+			   (vap->va_xflags & XFS_DIFLAG_REALTIME)) ) ) ) {
+			code = XFS_ERROR(EINVAL);
+			goto error_return;
+		}
+
 		/*
 		 * Can't change realtime flag if any extents are allocated.
 		 */
