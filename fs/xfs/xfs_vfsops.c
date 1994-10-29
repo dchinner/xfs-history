095a3279379d2353bc672ff1d2687a9073cbfbde
Michael Nishimoto <miken@sgi.com>
1994-10-29 02:01:05 +0000

    Deal with new fields passed in my mount command.  Store them in the mount structure.  Also reject old mount commands. Return EINVAL.

--- fs/xfs/xfs_vfsops.c
+++ fs/xfs/xfs_vfsops.c
@@ -16,7 +16,7 @@
  * successor clauses in the FAR, DOD or NASA FAR Supplement. Unpublished -
  * rights reserved under the Copyright Laws of the United States.
  */
-#ident  "$Revision: 1.65 $"
+#ident  "$Revision: 1.66 $"
 
 #include <strings.h>
 #include <sys/types.h>
@@ -368,11 +368,18 @@ xfs_cmountfs(struct vfs 	*vfsp,
 			}
 			mp->m_logdevp = ldevvp;
 		}
+		if (ap->version != 1) {
+			error = XFS_ERROR(EINVAL);
+			goto error;
+		}
+		mp->m_logbufs = ap->logbufs;
+		mp->m_logbsize = ap->logbufsize;
 	} else
 		ldevvp = NULL;
 
 	if (error = xfs_mountfs(vfsp, ddev)) {
 		xfs_mount_free(mp);
+error:
 		if (ldevvp) {
 			VOP_CLOSE(ldevvp, vfs_flags, 1, 0, cr);
 			binval(logdev);
