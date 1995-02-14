1400846c72ddf0b0d67871d6a18d647acb200c54
Adam Sweeney <ajs@sgi.com>
1995-02-14 22:20:35 +0000

    Use union rval pointers rather than rval_t pointers in prototypes so that we don't need to have the real declaration when it's not really used.

--- fs/xfs/xfs_handle.h
+++ fs/xfs/xfs_handle.h
@@ -1,7 +1,7 @@
 #ifndef _XFS_HANDLE_H
 #define _XFS_HANDLE_H
 
-#ident	"$Revision: 1.6 $"
+#ident	"$Revision: 1.7 $"
 
 /*
  *  Ok.  This file contains stuff that defines a general handle
@@ -20,13 +20,14 @@ typedef	struct handle {
 /*
  *  Kernel only prototypes.
  */
+union rval;
 
 int	path_to_handle		(char *path, void *hbuf, size_t *hlen);
 int	path_to_fshandle	(char *path, void *hbuf, size_t *hlen);
 int	fd_to_handle		(int	 fd, void *hbuf, size_t *hlen);
-int	open_by_handle		(void *hbuf, size_t hlen, int rw, rval_t *rvp);
+int	open_by_handle		(void *hbuf, size_t hlen, int rw, union rval *rvp);
 int	readlink_by_handle	(void *hanp, size_t hlen, void *buf, size_t bs,
-				 rval_t *rvp);
+				 union rval *rvp);
 int	gethandle		(void *hanp, size_t hlen, handle_t *handlep);
 
 struct vnode *	handle_to_vp (handle_t *handlep);
