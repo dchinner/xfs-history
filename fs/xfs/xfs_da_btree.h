0ab7a25ddaeae83019bb7fc0ae6494ebf3560415
Doug Doucette <doucette@engr.sgi.com>
1995-03-31 20:05:23 +0000

    Define typedef names for all the directory code's structs and use them. Use struct xxx * instead of xxx_t * in xfs header files to avoid unnecessary dependencies between header files.

--- fs/xfs/xfs_da_btree.h
+++ fs/xfs/xfs_da_btree.h
@@ -1,11 +1,19 @@
 #ifndef _FS_XFS_DIR_BTREE_H
 #define	_FS_XFS_DIR_BTREE_H
 
-#ident	"$Revision: 1.11 $"
+#ident	"$Revision: 1.12 $"
 
 /*
  * xfs_dir_btree.h
  */
+
+struct buf;
+struct dirent;
+struct uio;
+struct xfs_bmap_free;
+struct xfs_inode;
+struct xfs_mount;
+struct xfs_trans;
 
 /*========================================================================
  * Btree searching and modification structure definitions.
@@ -14,16 +22,16 @@
 /*
  * Structure to ease passing around component names.
  */
-struct xfs_dir_name {
+typedef struct xfs_dir_name {
 	char		*name;		/* string (maybe not NULL terminated) */
 	int		namelen;	/* length of string (maybe no NULL) */
 	uint		hashval;	/* hash value of name */
 	xfs_ino_t	inumber;	/* input/output inode number */
-	xfs_inode_t	*dp;		/* directory inode to manipulate */
+	struct xfs_inode *dp;		/* directory inode to manipulate */
 	xfs_fsblock_t	*firstblock;	/* ptr to firstblock for bmap calls */
-	xfs_bmap_free_t	*flist;		/* ptr to freelist for bmap_finish */
+	struct xfs_bmap_free *flist;	/* ptr to freelist for bmap_finish */
 	xfs_extlen_t	total;		/* total blocks needed, for 1st bmap */
-};
+} xfs_dir_name_t;
 
 /*
  * Storage for holding state during Btree searches and split/join ops.
@@ -32,26 +40,26 @@ struct xfs_dir_name {
  * fanout to the Btree, we can support over 900 million directory blocks,
  * which is slightly more than enough.
  */
-struct xfs_dir_state_blk {
-	buf_t		*bp;		/* buffer containing block */
+typedef struct xfs_dir_state_blk {
+	struct buf	*bp;		/* buffer containing block */
 	xfs_fileoff_t	blkno;		/* blkno of buffer */
 	int		index;		/* relevant index into block */
 	uint		hashval;	/* last hash value in block */
 	int		leafblk;	/* 1->blk is a leaf, 0->blk is a node */
-};
-struct xfs_dir_state_path {
+} xfs_dir_state_blk_t;
+typedef struct xfs_dir_state_path {
 	int			active;		/* number of active levels */
-	struct xfs_dir_state_blk blk[XFS_DIR_NODE_MAXDEPTH];
-};
-struct xfs_dir_state {
-	struct xfs_dir_name	  *args;	/* filename arguments */
-	xfs_mount_t		  *mp;		/* filesystem mount point */
-	xfs_trans_t		  *trans;	/* transaction context */
-	int			  blocksize;	/* logical block size */
-	int			  inleaf;	/* insert into 1->lf, 0->splf */
-	struct xfs_dir_state_path path;		/* search/split paths */
-	struct xfs_dir_state_path altpath;	/* alternate path for join */
-};
+	xfs_dir_state_blk_t	blk[XFS_DIR_NODE_MAXDEPTH];
+} xfs_dir_state_path_t;
+typedef struct xfs_dir_state {
+	xfs_dir_name_t		*args;		/* filename arguments */
+	struct xfs_mount	*mp;		/* filesystem mount point */
+	struct xfs_trans	*trans;		/* transaction context */
+	int			blocksize;	/* logical block size */
+	int			inleaf;		/* insert into 1->lf, 0->splf */
+	xfs_dir_state_path_t	path;		/* search/split paths */
+	xfs_dir_state_path_t	altpath;	/* alternate path for join */
+} xfs_dir_state_t;
 
 /*
  * Utility macros to aid in logging changed structure fields.
@@ -68,38 +76,39 @@ struct xfs_dir_state {
 /*
  * Routines used for growing the Btree.
  */
-int	xfs_dir_split(struct xfs_dir_state *state);
-int	xfs_dir_leaf_add(xfs_trans_t *trans, buf_t *leaf_buffer,
-				     struct xfs_dir_name *args,
-				     int insertion_index);
+int	xfs_dir_split(xfs_dir_state_t *state);
+int	xfs_dir_leaf_add(struct xfs_trans *trans, struct buf *leaf_buffer,
+			     xfs_dir_name_t *args, int insertion_index);
 
 /*
  * Routines used for shrinking the Btree.
  */
 #ifndef SIM
-int	xfs_dir_join(struct xfs_dir_state *state);
+int	xfs_dir_join(xfs_dir_state_t *state);
 #endif	/* !SIM */
-void	xfs_dir_fixhashpath(struct xfs_dir_state *state,
-				   struct xfs_dir_state_path *path_to_to_fix);
+void	xfs_dir_fixhashpath(xfs_dir_state_t *state,
+				   xfs_dir_state_path_t *path_to_to_fix);
 #ifndef SIM
-int	xfs_dir_leaf_remove(xfs_trans_t *trans, buf_t *leaf_buffer,
-					int index_to_remove);
+int	xfs_dir_leaf_remove(struct xfs_trans *trans, struct buf *leaf_buffer,
+				int index_to_remove);
 #endif	/* !SIM */
 
 /*
  * Routines used for finding things in the Btree.
  */
-int	xfs_dir_leaf_lookup_int(buf_t *leaf_buffer, struct xfs_dir_name *args,
+int	xfs_dir_leaf_lookup_int(struct buf *leaf_buffer, xfs_dir_name_t *args,
 				      int *index_found_at);
-int	xfs_dir_node_lookup_int(struct xfs_dir_state *state);
+int	xfs_dir_node_lookup_int(xfs_dir_state_t *state);
 
 #ifndef SIM
 /*
  * Routines used to print, getdents things from the Btree.
  */
-void	xfs_dir_leaf_print_int(buf_t *leaf_buffer, xfs_inode_t *dir_inode);
-int	xfs_dir_leaf_getdents_int(buf_t *bp, xfs_inode_t *dp, uio_t *uio,
-					int *eobp, dirent_t *dbp);
+void	xfs_dir_leaf_print_int(struct buf *leaf_buffer,
+				struct xfs_inode *dir_inode);
+int	xfs_dir_leaf_getdents_int(struct buf *bp, struct xfs_inode *dp,
+					struct uio *uio, int *eobp,
+					struct dirent *dbp);
 #endif	/* !SIM */
 
 #ifdef XFSDIRDEBUG
@@ -109,22 +118,24 @@ int	xfs_dir_leaf_getdents_int(buf_t *bp, xfs_inode_t *dp, uio_t *uio,
 #define xfs_trans_log_inode(T,I,F) xfsdir_t_log_inode(T,I,F,__FILE__,__LINE__)
 #define xfs_dir_read_buf(T,I,B) xfsdir_t_dir_read_buf(T,I,B,__FILE__,__LINE__)
 void xfsdir_t_reinit(char *description, char *file, int line);
-void xfsdir_t_binval(xfs_trans_t *tp, buf_t *bp, char *file, int line);
-void xfsdir_t_brelse(xfs_trans_t *tp, buf_t *bp, char *file, int line);
-void xfsdir_t_log_buf(xfs_trans_t *tp, buf_t *bp, uint first, uint last,
-				  char *file, int line);
-void xfsdir_t_log_inode(xfs_trans_t *tp, xfs_inode_t *ip, uint flags,
+void xfsdir_t_binval(struct xfs_trans *tp, struct buf *bp,
+			char *file, int line);
+void xfsdir_t_brelse(struct xfs_trans *tp, struct buf *bp,
+			char *file, int line);
+void xfsdir_t_log_buf(struct xfs_trans *tp, struct buf *bp,
+			uint first, uint last, char *file, int line);
+void xfsdir_t_log_inode(struct xfs_trans *tp, struct xfs_inode *ip, uint flags,
 				    char *file, int line);
-buf_t *xfsdir_t_dir_read_buf(xfs_trans_t *trans, xfs_inode_t *dp,
+struct buf *xfsdir_t_dir_read_buf(struct xfs_trans *trans, struct xfs_inode *dp,
 					 xfs_fileoff_t bno,
 					 char *file, int line);
 #define BUFTRACEMAX	128
-struct xfsdir_buftrace {
-	short	which;
-	short	line;
-	char	*file;
-	buf_t	*bp;
-};
+typedef struct xfsdir_buftrace {
+	short		which;
+	short		line;
+	char		*file;
+	struct buf	*bp;
+} xfsdir_buftrace_t;
 #endif /* XFSDIRDEBUG */
 
 #endif	/* !FS_XFS_DIR_BTREE_H */
