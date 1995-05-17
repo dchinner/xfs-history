cbadf19a2241c0ccb13adcd863f645d98b9e6f95
Curtis Anderson <curtis@sgi.com>
1995-05-17 23:08:00 +0000

    Add a flags byte to each entry for root-only attributes and future growth.

--- fs/xfs/xfs_attr_sf.h
+++ fs/xfs/xfs_attr_sf.h
@@ -23,6 +23,7 @@ typedef struct xfs_attr_shortform {
 	struct xfs_attr_sf_entry {
 		__uint8_t namelen;	/* actual length of name (no NULL) */
 		__uint8_t valuelen;	/* actual length of value (no NULL) */
+		__uint8_t flags;	/* flags bits (see xfs_attr_leaf.h) */
 		__uint8_t nameval[1];	/* name & value bytes concatenated */
 	} list[1];			/* variable sized array */
 } xfs_attr_shortform_t;
@@ -32,11 +33,13 @@ typedef struct xfs_attr_sf_entry xfs_attr_sf_entry_t;
 #define XFS_ATTR_SF_ENTSIZE_BYNAME(NLEN, VLEN)	/* space name/value uses */ \
 	(sizeof(xfs_attr_sf_entry_t)-1 + (NLEN)+(VLEN))
 #define XFS_ATTR_SF_ENTSIZE_MAX			/* max space for name&value */ \
-	((1 << sizeof(xfs_attr_sf_entry_t.namelen)) - 2)
+	((1 << (NBBY*sizeof(__uint8_t))) - 1)
 #define XFS_ATTR_SF_ENTSIZE(SFEP)		/* space an entry uses */ \
 	(sizeof(xfs_attr_sf_entry_t)-1 + (SFEP)->namelen+(SFEP)->valuelen)
 #define XFS_ATTR_SF_NEXTENTRY(SFEP)		/* next entry in struct */ \
 	((xfs_attr_sf_entry_t *) \
 		((char *)(SFEP) + XFS_ATTR_SF_ENTSIZE(SFEP)))
+#define XFS_ATTR_SF_TOTSIZE(DP)			/* total space in use */ \
+	(((xfs_attr_shortform_t *)((DP)->i_af.if_u1.if_data))->hdr.totsize)
 
 #endif	/* !FS_XFS_ATTR_SF_H */
