a30844235d9e4d6fab511ac5b6b76d1a5ba142ed
Doug Doucette <doucette@engr.sgi.com>
1995-05-24 22:29:27 +0000

    Move the attrlist_cursor and attr_multiop_kern structures into sys/attributes.h under #ifdef _KERNEL, since they're used in os/vncalls.c and fs/specfs/specvnops.c.

--- fs/xfs/xfs_attr.h
+++ fs/xfs/xfs_attr.h
@@ -1,7 +1,7 @@
 #ifndef _FS_XFS_ATTR_H
 #define	_FS_XFS_ATTR_H
 
-#ident	"$Revision: 1.20 $"
+#ident	"$Revision: 1.3 $"
 
 /*
  * xfs_attr.h
@@ -16,48 +16,10 @@
  * as possible so as to fit into the literal area of the inode.
  */
 
-/*
- * Provide a way to find the last attribute that was returned in the
- * last syscall so that we can get the next one without missing any.
- */
-struct attrlist_cursor_kern {
-	u_int32_t	hashval;	/* hash value of this entry */
-	u_int32_t	blkno;		/* block containing entry */
-	u_int32_t	index;		/* index of entry in block */
-	u_int16_t	pad1;		/* padding to match user-level */
-	u_int8_t	pad2;		/* padding to match user-level */
-	u_int8_t	initted;	/* T/F: cursor has been initialized */
-};
-typedef struct attrlist_cursor_kern attrlist_cursor_kern_t;
-
-/*
- * Kernel versions of the multi-attribute operation structure,
- * one for each of the 32bit and 64bit ABIs.
- */
-struct attr_multiop_kern_32 {
-	__int32_t	am_opcode;	/* which operation to perform */
-	__int32_t	am_error;	/* [out arg] result of this sub-op */
-	app32_ptr_t	am_attrname;	/* attribute name to work with */
-	app32_ptr_t	am_attrvalue;	/* [in/out arg] attribute value */
-	__int32_t	am_length;	/* [in/out arg] length of value */
-	__int32_t	am_flags;	/* flags (bit-wise OR of lists below) */
-	
-};
-struct attr_multiop_kern_64 {
-	__int32_t	am_opcode;	/* which operation to perform */
-	__int32_t	am_error;	/* [out arg] result of this sub-op */
-	app64_ptr_t	am_attrname;	/* attribute name to work with */
-	app64_ptr_t	am_attrvalue;	/* [in/out arg] attribute value */
-	__int32_t	am_length;	/* [in/out arg] length of value */
-	__int32_t	am_flags;	/* flags (bit-wise OR of lists below) */
-	
-};
-
 
 /*========================================================================
  * Function prototypes for the kernel.
  *========================================================================*/
-
 
 struct cred;
 struct vnode;
