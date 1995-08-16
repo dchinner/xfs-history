2f69886b615a28f0e4aeb8899ef0479ff65ab9ef
Doug Doucette <doucette@engr.sgi.com>
1995-08-16 05:45:52 +0000

    Move xfs_lowbit32 and xfs_highbit64 to new file xfs_rtbit.c, since they're only used by xfsrt.a code.

--- fs/xfs/xfs_bit.c
+++ fs/xfs/xfs_bit.c
@@ -1,7 +1,7 @@
-#ident "$Revision: 1.1 $"
+#ident "$Revision: 1.2 $"
 
 /*
- * XFS bit manipulation routines.
+ * XFS bit manipulation routines, used in non-realtime code.
  */
 
 #include <sys/types.h>
@@ -122,27 +122,6 @@ const char xfs_countbit[256] = {
 };
 
 /*
- * xfs_lowbit32: get low bit set out of 32-bit argument, -1 if none set.
- */
-int
-xfs_lowbit32(
-	__uint32_t	v)
-{
-	if (v & 0x0000ffff)
-		if (v & 0x000000ff)
-			return xfs_lowbit[v & 0xff];
-		else
-			return 8 + xfs_lowbit[(v >> 8) & 0xff];
-	else if (v & 0xffff0000)
-		if (v & 0x00ff0000)
-			return 16 + xfs_lowbit[(v >> 16) & 0xff];
-		else
-			return 24 + xfs_lowbit[(v >> 24) & 0xff];
-	else
-		return -1;
-}
-
-/*
  * xfs_highbit32: get high bit set out of 32-bit argument, -1 if none set.
  */
 int
@@ -220,68 +199,6 @@ xfs_lowbit64(
 				return 48 + xfs_lowbit[(vw >> 16) & 0xff];
 			else
 				return 56 + xfs_lowbit[(vw >> 24) & 0xff];
-	else
-		return -1;
-#endif
-}
-
-/*
- * xfs_highbit64: get high bit set out of 64-bit argument, -1 if none set.
- */
-int
-xfs_highbit64(
-	__uint64_t	v)
-{
-#if (_MIPS_SZLONG == 64)
-	if (v & 0xffffffff00000000)
-		if (v & 0xffff000000000000)
-			if (v & 0xff00000000000000)
-				return 56 + xfs_highbit[(v >> 56) & 0xff];
-			else
-				return 48 + xfs_highbit[(v >> 48) & 0xff];
-		else
-			if (v & 0x0000ff0000000000)
-				return 40 + xfs_highbit[(v >> 40) & 0xff];
-			else
-				return 32 + xfs_highbit[(v >> 32) & 0xff];
-	else if (v & 0x00000000ffffffff)
-		if (v & 0x00000000ffff0000)
-			if (v & 0x00000000ff000000)
-				return 24 + xfs_highbit[(v >> 24) & 0xff];
-			else
-				return 16 + xfs_highbit[(v >> 16) & 0xff];
-		else
-			if (v & 0x000000000000ff00)
-				return 8 + xfs_highbit[(v >> 8) & 0xff];
-			else
-				return xfs_highbit[v & 0xff];
-	else
-		return -1;
-#else
-	__uint32_t	vw;
-
-	if (vw = v >> 32)
-		if (vw & 0xffff0000)
-			if (vw & 0xff000000)
-				return 56 + xfs_highbit[(vw >> 24) & 0xff];
-			else
-				return 48 + xfs_highbit[(vw >> 16) & 0xff];
-		else
-			if (vw & 0x0000ff00)
-				return 40 + xfs_highbit[(vw >> 8) & 0xff];
-			else
-				return 32 + xfs_highbit[vw & 0xff];
-	else if (vw = v)
-		if (vw & 0xffff0000)
-			if (vw & 0xff000000)
-				return 24 + xfs_highbit[(vw >> 24) & 0xff];
-			else
-				return 16 + xfs_highbit[(vw >> 16) & 0xff];
-		else
-			if (vw & 0x0000ff00)
-				return 8 + xfs_highbit[(vw >> 8) & 0xff];
-			else
-				return xfs_highbit[vw & 0xff];
 	else
 		return -1;
 #endif
