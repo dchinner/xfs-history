fbd07221dffd887c9bbc10ea95eed2a16ee22892
Doug Doucette <doucette@engr.sgi.com>
1995-08-25 20:25:17 +0000

    Turn on XFS_BIG_FILESYSTEMS for 64-bit platforms.

--- fs/xfs/xfs_types.h
+++ fs/xfs/xfs_types.h
@@ -1,7 +1,7 @@
 #ifndef _FS_XFS_TYPES_H
 #define	_FS_XFS_TYPES_H
 
-#ident	"$Revision: 1.24 $"
+#ident	"$Revision: 1.25 $"
 
 /*
  * XFS types
@@ -27,7 +27,7 @@
 #endif
 
 #ifndef XFS_BIG_FILESYSTEMS
-#if defined(_K32U32) || defined(_K32U64) || defined(_K64U64)
+#if defined(_K32U32) || defined(_K32U64)
 #define	XFS_BIG_FILESYSTEMS	0
 #else
 #define	XFS_BIG_FILESYSTEMS	1
