a59593060708c9b401844d98754c9baed05e02f4
Supriya Wickrematillake <sup@sgi.com>
1996-05-15 21:53:50 +0000

    XFSMNT options for quotas

--- fs/xfs/xfs_clnt.h
+++ fs/xfs/xfs_clnt.h
@@ -12,7 +12,7 @@
  *  in part, without the prior written consent of Silicon Graphics, Inc.  *
  *									  *
  **************************************************************************/
-#ident "$Revision: 1.5 $"
+#ident "$Revision: 1.6 $"
 
 /*
  * XFS arguments to the mount system call.
@@ -41,5 +41,9 @@ struct irix5_xfs_args {
 #define	XFSMNT_CHKLOG	0x0001	/* check log */
 #define	XFSMNT_WSYNC	0x0002	/* safe mode nfs mount compatible */
 #define	XFSMNT_INO64	0x0004	/* move inode numbers up past 2^32 */
+#define XFSMNT_UQUOTA		0x0008	/* user quota accounting */
+#define XFSMNT_PQUOTA		0x0010	/* project quota accounting */
+#define XFSMNT_UQUOTAENF	0x0020	/* user quota limit enforcement */
+#define XFSMNT_PQUOTAENF	0x0040	/* project quota limit enforcement */
 
 #endif /* !__SYS_XFS_CLNT_H__ */
