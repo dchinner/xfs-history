c13911559e64e68c4d4bea92f96df4f33d5b1e10
Supriya Wickrematillake <sup@sgi.com>
1996-05-15 22:41:59 +0000

    Initial checkin. Handles both LI_DQUOT and LI_QUOTAOFF items for quotas.

---
+++ fs/xfs/xfs_dquot_item.c
@@ -0,0 +1,604 @@
+
+#ifdef SIM
+#define _KERNEL 1
+#endif
+#include <sys/param.h>
+#include <sys/buf.h>
+#include <sys/vnode.h>
+#include <sys/uuid.h>
+#ifdef SIM
+#undef _KERNEL
+#endif
+#include <sys/kmem.h>
+#include <sys/debug.h>
+#include <sys/proc.h>
+#include <sys/errno.h>
+#include <sys/vfs.h>
+#ifdef SIM
+#include <bstring.h>
+#else
+#include <sys/systm.h>
+#endif
+#include <sys/atomic_ops.h>
+
+#include "xfs_macros.h"
+#include "xfs_types.h"
+#include "xfs_inum.h"
+#include "xfs_log.h"
+#include "xfs_trans.h"
+#include "xfs_sb.h"
+#include "xfs_mount.h"
+#include "xfs_alloc_btree.h"
+#include "xfs_bmap_btree.h"
+#include "xfs_ialloc_btree.h"
+#include "xfs_ialloc.h"
+#include "xfs_alloc.h"
+#include "xfs_bmap.h"
+#include "xfs_btree.h"
+#include "xfs_bmap.h"
+#include "xfs_attr_sf.h"
+#include "xfs_dir_sf.h"
+#include "xfs_dinode.h"
+#include "xfs_inode_item.h"
+#include "xfs_da_btree.h"
+#include "xfs_inode.h"
+#include "xfs_trans_priv.h"
+#include "xfs_error.h"
+#include "xfs_quota.h"
+#include "xfs_dquot.h"
+#include "xfs_qm.h"
+#include "xfs_quota_priv.h"
+
+
+#ifdef SIM
+#include "sim.h"
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+STATIC uint		xfs_qm_dquot_logitem_size(xfs_dq_logitem_t *logitem);
+STATIC void		xfs_qm_dquot_logitem_format(xfs_dq_logitem_t *logitem,
+					      xfs_log_iovec_t *logvec);
+STATIC void		xfs_qm_dquot_logitem_unpin(xfs_dq_logitem_t *logitem);
+STATIC xfs_lsn_t 	xfs_qm_dquot_logitem_committed(xfs_dq_logitem_t *logitem, 
+						 xfs_lsn_t lsn);
+STATIC void		xfs_qm_dquot_logitem_push(xfs_dq_logitem_t *logitem);
+
+
+STATIC uint		xfs_qm_qoff_logitem_size(xfs_qoff_logitem_t *logitem);
+STATIC void		xfs_qm_qoff_logitem_format(xfs_qoff_logitem_t *logitem,
+					      xfs_log_iovec_t *logvec);
+STATIC void		xfs_qm_qoff_logitem_unpin(xfs_qoff_logitem_t *logitem);
+STATIC xfs_lsn_t 	xfs_qm_qoff_logitem_committed(xfs_qoff_logitem_t *logitem, 
+						 xfs_lsn_t lsn);
+STATIC void		xfs_qm_qoff_logitem_push(xfs_qoff_logitem_t *logitem);
+
+STATIC xfs_lsn_t	xfs_qm_qoffend_logitem_committed(xfs_qoff_logitem_t *qfe,
+							 xfs_lsn_t lsn);
+
+/*
+ * returns the number of iovecs needed to log the given dquot item.
+ */
+/* ARGSUSED */
+STATIC uint
+xfs_qm_dquot_logitem_size(
+	xfs_dq_logitem_t	*logitem)
+{
+	/* 
+	 * we need only two iovecs, one for the format, one for the real thing 
+	 */
+	return (2);
+}
+
+/*
+ * fills in the vector of log iovecs for the given dquot log item.
+ */
+STATIC void
+xfs_qm_dquot_logitem_format(
+	xfs_dq_logitem_t	*logitem,
+	xfs_log_iovec_t		*logvec)
+{
+	ASSERT(logitem);
+	ASSERT(logitem->qli_dquot);
+
+	logvec->i_addr = (caddr_t)&logitem->qli_format;
+	logvec->i_len  = sizeof(xfs_dq_logformat_t);
+	logvec++;
+	logvec->i_addr = (caddr_t)&logitem->qli_dquot->q_core;
+	logvec->i_len  = sizeof(xfs_disk_dquot_t);
+
+	ASSERT(2 == logitem->qli_item.li_desc->lid_size);
+	logitem->qli_format.qlf_size = 2;
+	
+}
+
+/*
+ * pin dquot associated with the given logitem
+ */
+STATIC void
+xfs_qm_dquot_logitem_pin(
+	xfs_dq_logitem_t *logitem)
+{
+	ASSERT(XFS_DQ_IS_LOCKED(logitem->qli_dquot));
+	xfs_qm_dqpin(logitem->qli_dquot);
+}
+
+STATIC void
+xfs_qm_dquot_logitem_unpin(
+	xfs_dq_logitem_t *logitem)
+{
+	xfs_qm_dqunpin(logitem->qli_dquot);
+}
+
+/*
+ * Given the logitem, this writes the corresponding dquot entry to disk
+ * asynchronously. This is called with the dquot entry securely locked;
+ * we simply get xfs_qm_dqflush() to do the work, and unlock the dquot
+ * at the end.
+ */
+STATIC void
+xfs_qm_dquot_logitem_push(
+	xfs_dq_logitem_t	*logitem)
+{
+	buf_t           *bp;
+	xfs_dquot_t	*dqp;
+
+	ASSERT(logitem->qli_bp != NULL || 
+	       XFS_DQ_IS_LOCKED(logitem->qli_dquot));
+	ASSERT(logitem->qli_bp != NULL || 
+	       XFS_DQ_IS_FLUSH_LOCKED(logitem->qli_dquot));  
+     
+	/*
+         * If the dquot item's buffer pointer is non-NULL, then we were
+         * unable to lock the dquot in the trylock routine but we were
+         * able to lock the dquot's buffer and it was marked delayed write.
+         * We didn't want to write it out from the trylock routine, because
+         * the device strategy routine might sleep.  Instead we stored it
+         * and write it out here.
+         */
+        bp = logitem->qli_bp;
+        dqp = logitem->qli_dquot;
+        if ((bp != NULL) && (logitem->qli_bp_owner == curprocp)) {
+                ASSERT(logitem->qli_bp->b_flags & B_BUSY);
+                logitem->qli_bp = NULL;
+                logitem->qli_bp_owner = NULL;
+#ifndef SIM
+                buftrace("DQUOT ITEM PUSH", bp);
+#endif
+                /*
+                 * If the buffer is pinned then push on the log so we won't
+                 * deadlock waiting for the buffer to be unpinned while we
+                 * hold other locks.
+                 */
+                if (bp->b_pincount > 0) {
+                        xfs_log_force(dqp->q_mount, (xfs_lsn_t)0,
+                                      XFS_LOG_FORCE);
+                }
+#ifdef SIM
+		bwrite(bp);
+#else
+                bawrite(bp);
+#endif
+                return;
+        }
+        /*
+         * Since we were able to lock the dquot's flush lock and
+         * we found it on the AIL, the dquot must be dirty.  This
+         * is because the dquot is removed from the AIL while still
+         * holding the flush lock in xfs_dqflush_done().  Thus, if
+         * we found it in the AIL and were able to obtain the flush
+         * lock without sleeping, then there must not have been
+         * anyone in the process of flushing the dquot.
+         */
+	xfs_qm_dqflush(dqp, B_DELWRI);
+	xfs_dqunlock(dqp);
+
+}
+
+/*ARGSUSED*/
+STATIC xfs_lsn_t
+xfs_qm_dquot_logitem_committed(
+	xfs_dq_logitem_t	*l,
+	xfs_lsn_t		lsn)
+{
+	/*
+	 * We always re-log the entire dquot when it becomes dirty,
+	 * so, the latest copy _is_ the only one that matters.
+	 */
+	return (lsn);
+}
+
+
+/*
+ * Increment the pin count of the given dquot.
+ * This value is protected by pinlock spinlock in the xQM structure.
+ */
+void
+xfs_qm_dqpin(
+	xfs_dquot_t 	*dqp)
+{
+	int 	s;
+
+	ASSERT(XFS_DQ_IS_LOCKED(dqp));
+	s = XFS_DQ_PINLOCK(dqp);
+	dqp->q_pincount++;
+	XFS_DQ_PINUNLOCK(dqp, s);
+}
+
+
+/*
+ * XXX why have two _qm_ and _item_ interfaces ???
+ * Decrement the pin count of the given dquot, and wake up
+ * anyone in xfs_dqwait_unpin() if the count goes to 0.  The
+ * dquot must have been previously pinned with a call to xfs_dqpin().
+ */
+void
+xfs_qm_dqunpin(
+	xfs_dquot_t 	*dqp)
+{
+	int 	s;
+
+	ASSERT(dqp->q_pincount > 0);
+	s = XFS_DQ_PINLOCK(dqp);
+	dqp->q_pincount--;
+	if (dqp->q_pincount == 0) {
+		sv_broadcast(&dqp->q_pinwait);
+	}
+	XFS_DQ_PINUNLOCK(dqp, s);
+}
+
+/*
+ * This is called to wait for the given dquot to be unpinned.
+ * Most of these pin/unpin routines are plagiarized from inode code.
+ */
+void
+xfs_qm_dqunpin_wait(
+	xfs_dquot_t	*dqp)
+{
+	int             s;
+	
+	ASSERT(XFS_DQ_IS_LOCKED(dqp));
+	if (dqp->q_pincount == 0) {
+		return;
+	}
+
+	/*
+	 * Give the log a push so we don't wait here too long.
+	 */
+	xfs_log_force(dqp->q_mount, (xfs_lsn_t)0, XFS_LOG_FORCE);
+	s = XFS_DQ_PINLOCK(dqp);
+	if (dqp->q_pincount == 0) {
+		XFS_DQ_PINUNLOCK(dqp, s);
+		return;
+	}
+	sv_wait(&(dqp->q_pinwait), PINOD,
+		&(XFS_DQ_TO_QINF(dqp)->qi_pinlock), s);
+	return;
+}
+
+/*
+ * This is called to attempt to lock the dquot associated with this
+ * dquot log item.  Don't sleep on the dquot lock or the flush lock.
+ * If the flush lock is already held, indicating that the dquot has
+ * been or is in the process of being flushed, then see if we can
+ * find the dquot's buffer in the buffer cache without sleeping.  If
+ * we can and it is marked delayed write, then we want to send it out.
+ * We delay doing so until the push routine, though, to avoid sleeping
+ * in any device strategy routines.
+ */
+STATIC uint
+xfs_qm_dquot_logitem_trylock(
+        xfs_dq_logitem_t    *qip)
+{
+	xfs_dquot_t	        *dqp;
+	xfs_mount_t             *mp;
+        buf_t                   *bp;
+        int                     flushed;
+
+	dqp = qip->qli_dquot;
+	if (dqp->q_pincount > 0)
+		return (XFS_ITEM_PINNED);
+	
+	if (! xfs_qm_dqlock_nowait(dqp))
+		return (XFS_ITEM_LOCKED);
+	
+	if (! xfs_qm_dqflock_nowait(dqp)) {
+		/*
+                 * The dquot is already being flushed.  It may have been
+                 * flushed delayed write, however, and we don't want to
+                 * get stuck waiting for that to complete.  So, we check
+                 * to see if we can lock the dquot's buffer without sleeping.
+                 * If we can and it is marked for delayed write, then we
+                 * hold it and send it out from the push routine.  We don't
+                 * want to do that now since we might sleep in the device
+                 * strategy routine.  Make sure to only return success
+                 * if we set qip->qli_bp ourselves.  If someone else is
+                 * doing it then we don't want to go to the push routine
+                 * and duplicate their efforts.
+                 */
+                flushed = 0;
+                if (qip->qli_bp == NULL) {
+                        mp = dqp->q_mount;
+                        bp = incore(mp->m_dev, dqp->q_blkno, 
+				    XFS_FSB_TO_BB(mp, 1),
+				    INCORE_TRYLOCK);
+                        if (bp != NULL) {
+                                if (bp->b_flags & B_DELWRI) {
+                                        qip->qli_bp = bp;
+                                        qip->qli_bp_owner = curprocp;
+                                        flushed = 1;
+                                } else {
+                                        brelse(bp);
+                                }
+                        }
+                }
+		xfs_dqunlock_nonotify(dqp);
+		return (flushed ? XFS_ITEM_SUCCESS : XFS_ITEM_FLUSHING);
+	}
+	return (XFS_ITEM_SUCCESS);
+}
+
+
+/*
+ * Unlock the dquot associated with the log item.
+ * Clear the fields of the dquot and dquot log item that
+ * are specific to the current transaction.  If the
+ * hold flags is set, do not unlock the dquot.  
+ */
+STATIC void
+xfs_qm_dquot_logitem_unlock(
+        xfs_dq_logitem_t    *ql)
+{
+	xfs_dquot_t	*dqp;
+
+	ASSERT(ql != NULL);
+	dqp = ql->qli_dquot;
+	ASSERT(XFS_DQ_IS_LOCKED(dqp));
+
+	/*
+         * Clear the transaction pointer in the dquot
+         */
+	dqp->q_transp = NULL;
+
+	/*
+	 * dquots are never 'held' from getting unlocked at the end of
+	 * a transaction.  Their locking and unlocking is hidden inside the
+	 * transaction layer, within trans_commit. Hence, no LI_HOLD flag
+	 * for the logitem.
+	 */
+	xfs_dqunlock(dqp);
+	ql->qli_flags = 0;	
+}
+
+
+/*
+ * The transaction with the dquot locked has aborted.  The dquot
+ * must not be dirty within the transaction.  We simply unlock just
+ * as if the transaction had been cancelled.
+ */
+STATIC void
+xfs_qm_dquot_logitem_abort(
+        xfs_dq_logitem_t    *ql)
+{
+        xfs_qm_dquot_logitem_unlock(ql);
+        return;
+}
+
+
+/*
+ * This is the ops vector for dquots
+ */
+struct xfs_item_ops xfs_dquot_item_ops = {
+	(uint(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_size,
+	(void(*)(xfs_log_item_t*, xfs_log_iovec_t*))xfs_qm_dquot_logitem_format,
+	(void(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_pin,
+	(void(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_unpin,
+	(uint(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_trylock,
+	(void(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_unlock,
+	(xfs_lsn_t(*)(xfs_log_item_t*, xfs_lsn_t))xfs_qm_dquot_logitem_committed,
+	(void(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_push,
+	(void(*)(xfs_log_item_t*))xfs_qm_dquot_logitem_abort
+};
+
+/*
+ * Initialize the dquot log item for a newly allocated dquot.
+ * The dquot isn't locked at this point, but it isn't on any of the lists
+ * either, so we don't care.
+ */
+void
+xfs_qm_dquot_logitem_init(
+	xfs_dquot_t	*dqp)
+{
+	xfs_dq_logitem_t  *lp;
+	lp = &dqp->q_logitem;
+
+	lp->qli_item.li_type = XFS_LI_DQUOT;
+        lp->qli_item.li_ops = &xfs_dquot_item_ops;
+        lp->qli_item.li_mountp = dqp->q_mount;
+	lp->qli_dquot = dqp;
+        lp->qli_bp = NULL;
+        lp->qli_format.qlf_type = XFS_LI_DQUOT;
+        lp->qli_format.qlf_id = dqp->q_core.d_id;
+        lp->qli_format.qlf_blkno = dqp->q_blkno;
+        lp->qli_format.qlf_len = 1;
+        lp->qli_format.qlf_boffset = dqp->q_bufoffset;
+}
+	
+/*------------------  QUOTAOFF LOG ITEMS  -------------------*/
+
+/*
+ * This returns the number of iovecs needed to log the given quotaoff item.
+ * We only need 1 iovec for an quotaoff item.  It just logs the quotaoff_log_format
+ * structure.
+ */
+/*ARGSUSED*/
+STATIC uint
+xfs_qm_qoff_logitem_size(xfs_qoff_logitem_t *qf)
+{
+	return (1);
+}
+
+/*
+ * This is called to fill in the vector of log iovecs for the
+ * given quotaoff log item. We use only 1 iovec, and we point that
+ * at the quotaoff_log_format structure embedded in the quotaoff item.
+ * It is at this point that we assert that all of the extent
+ * slots in the quotaoff item have been filled.
+ */
+STATIC void
+xfs_qm_qoff_logitem_format(xfs_qoff_logitem_t	*qf,
+			   xfs_log_iovec_t	*log_vector)
+{
+	ASSERT(qf->qql_format.qf_type == XFS_LI_QUOTAOFF);
+
+	log_vector->i_addr = (caddr_t)&(qf->qql_format);
+	log_vector->i_len = sizeof(xfs_qoff_logitem_t);
+	qf->qql_format.qf_size = 1;
+}
+	
+
+/*
+ * Pinning has no meaning for an quotaoff item, so just return.
+ */
+/*ARGSUSED*/
+STATIC void
+xfs_qm_qoff_logitem_pin(xfs_qoff_logitem_t *qf)
+{
+	return;
+}
+
+
+/*
+ * Since pinning has no meaning for an quotaoff item, unpinning does
+ * not either.
+ */
+/*ARGSUSED*/
+STATIC void
+xfs_qm_qoff_logitem_unpin(xfs_qoff_logitem_t *qf)
+{
+	return;
+}
+
+/*
+ * Quotaoff items have no locking, so just return success.
+ */
+/*ARGSUSED*/
+STATIC uint
+xfs_qm_qoff_logitem_trylock(xfs_qoff_logitem_t *qf)
+{
+	return XFS_ITEM_LOCKED;
+}
+
+/*
+ * Quotaoff items have no locking or pushing, so return failure
+ * so that the caller doesn't bother with us.
+ */
+/*ARGSUSED*/
+STATIC void
+xfs_qm_qoff_logitem_unlock(xfs_qoff_logitem_t *qf)
+{
+	return;
+}
+
+/*
+ * The quotaoff-start-item is logged only once and cannot be moved in the log, so
+ * simply return the lsn at which it's been logged.  
+ */
+/*ARGSUSED*/
+STATIC xfs_lsn_t
+xfs_qm_qoff_logitem_committed(xfs_qoff_logitem_t *qf, xfs_lsn_t lsn)
+{
+	return (lsn);
+}
+
+/*
+ * The transaction of which this QUOTAOFF is a part has been aborted.
+ * Just clean up after ourselves.
+ * Shouldn't this never happen in the case of qoffend logitems? XXX
+ */
+STATIC void
+xfs_qm_qoff_logitem_abort(xfs_qoff_logitem_t *qf)
+{
+	kmem_free(qf, sizeof(xfs_qoff_logitem_t));
+}
+
+/*
+ * There isn't much you can do to push on an quotaoff item.  It is simply
+ * stuck waiting for the log to be flushed to disk.
+ */
+/*ARGSUSED*/
+STATIC void
+xfs_qm_qoff_logitem_push(xfs_qoff_logitem_t *qf)
+{
+	return;
+}
+
+
+/*ARGSUSED*/
+STATIC xfs_lsn_t
+xfs_qm_qoffend_logitem_committed(xfs_qoff_logitem_t *qfe, xfs_lsn_t lsn)
+{
+	int			s;
+	xfs_qoff_logitem_t 	*qfs;
+	
+	qfs = qfe->qql_start_lip;
+	s = AIL_LOCK(qfs->qql_item.li_mountp);
+	/*
+	 * Delete the qoff-start logitem from the AIL.
+	 * xfs_trans_delete_ail() drops the AIL lock.
+	 */
+	xfs_trans_delete_ail(qfs->qql_item.li_mountp, (xfs_log_item_t *)qfs, s);
+	kmem_free(qfs, sizeof(xfs_qoff_logitem_t));
+	kmem_free(qfe, sizeof(xfs_qoff_logitem_t));
+	return (xfs_lsn_t)-1;
+}
+
+struct xfs_item_ops xfs_qm_qoffend_logitem_ops = {
+	(uint(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_size,
+	(void(*)(xfs_log_item_t*, xfs_log_iovec_t*))xfs_qm_qoff_logitem_format,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_pin,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_unpin,
+	(uint(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_trylock,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_unlock,
+	(xfs_lsn_t(*)(xfs_log_item_t*, xfs_lsn_t))xfs_qm_qoffend_logitem_committed,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_push,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_abort
+};
+
+/*
+ * This is the ops vector shared by all quotaoff-start log items.
+ */
+struct xfs_item_ops xfs_qm_qoff_logitem_ops = {
+	(uint(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_size,
+	(void(*)(xfs_log_item_t*, xfs_log_iovec_t*))xfs_qm_qoff_logitem_format,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_pin,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_unpin,
+	(uint(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_trylock,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_unlock,
+	(xfs_lsn_t(*)(xfs_log_item_t*, xfs_lsn_t))xfs_qm_qoff_logitem_committed,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_push,
+	(void(*)(xfs_log_item_t*))xfs_qm_qoff_logitem_abort
+};
+
+/*
+ * Allocate and initialize an quotaoff item of the correct quota type(s).
+ */
+xfs_qoff_logitem_t *
+xfs_qm_qoff_logitem_init(xfs_mount_t *mp, xfs_qoff_logitem_t *start, uint flags)
+{
+	xfs_qoff_logitem_t	*qf;
+
+	qf = (xfs_qoff_logitem_t*) kmem_zalloc(sizeof(xfs_qoff_logitem_t), KM_SLEEP);
+
+	qf->qql_item.li_type = XFS_LI_QUOTAOFF;
+	if (start) 
+		qf->qql_item.li_ops = &xfs_qm_qoffend_logitem_ops;
+	else
+		qf->qql_item.li_ops = &xfs_qm_qoff_logitem_ops;
+	qf->qql_item.li_mountp = mp;
+	qf->qql_format.qf_type = XFS_LI_QUOTAOFF;
+	qf->qql_format.qf_flags = flags;
+	qf->qql_start_lip = start;
+	return (qf);
+}
+
