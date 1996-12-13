36ed4aca763b11a7ff0282341e3fe3cebabbca79
Supriya Wickrematillake <sup@sgi.com>
1996-12-13 00:46:23 +0000

    Common routines and there defines that used to live in xfs_vnodeops.c

---
+++ fs/xfs/xfs_utils.h
@@ -0,0 +1,76 @@
+#ifndef __XFS_UTILS_H__
+#define __XFS_UTILS_H__
+
+#define IRELE(ip)	VN_RELE(XFS_ITOV(ip))
+#define IHOLD(ip)	VN_HOLD(XFS_ITOV(ip))
+#define	ITRACE(ip)	vn_trace_ref(XFS_ITOV(ip), __FILE__, __LINE__, \
+				(inst_t *)__return_address)
+
+#define DLF_IGET	0x01	/* get entry inode if name lookup succeeds */
+#define	DLF_NODNLC	0x02	/* don't use the dnlc */
+#define	DLF_LOCK_SHARED	0x04	/* directory locked shared */
+
+/*
+ * Max number of extents needed for directory create + symlink.
+ */
+#define MAX_EXT_NEEDED 9
+
+#ifndef SIM
+extern int
+xfs_rename(
+	bhv_desc_t	*src_dir_bdp,
+	char		*src_name,
+	vnode_t		*target_dir_vp,
+	char		*target_name,
+	struct pathname	*target_pnp,
+	cred_t		*credp);
+
+extern int
+xfs_dir_lookup_int(
+	xfs_trans_t  		*tp,
+	bhv_desc_t     		*dir_bdp,
+	int		 	flags,
+	char         		*name,
+	struct pathname   	*pnp,
+	xfs_ino_t    		*inum,
+	xfs_inode_t  		**ipp,
+	struct ncfastdata	*fd,
+	uint			*dir_unlocked);
+
+extern int
+xfs_truncate_file(
+	xfs_mount_t	*mp,
+	xfs_inode_t	*ip);
+
+extern int 	
+xfs_dir_ialloc(
+	xfs_trans_t 	**tpp, 
+	xfs_inode_t 	*dp, 
+	mode_t 		mode, 
+	nlink_t 	nlink, 
+	dev_t 		rdev,
+	cred_t 		*credp,
+	prid_t 		prid,
+	xfs_inode_t	**ipp,
+	int 		*committed);
+
+extern int
+xfs_stickytest(
+	xfs_inode_t	*dp,
+	xfs_inode_t	*ip,
+	cred_t		*cr);
+
+extern int
+xfs_droplink(
+	xfs_trans_t	*tp,
+	xfs_inode_t	*ip);
+
+extern int
+xfs_bumplink(
+	xfs_trans_t	*tp,
+	xfs_inode_t	*ip);
+
+#endif /* !SIM */
+
+#endif /* XFS_UTILS_H */
+
