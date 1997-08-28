e71b0933426f77b3ce01e8710057f351ca3c62fb
Steve Lord <lord@sgi.com>
1997-08-28 16:26:15 +0000

    Add space reservation macros for attribute setting

--- fs/xfs/xfs_trans_space.h
+++ fs/xfs/xfs_trans_space.h
@@ -1,12 +1,18 @@
 #ifndef _XFS_TRANS_SPACE_H
 #define _XFS_TRANS_SPACE_H
 
-#ident "$Revision: 1.1 $"
+#ident "$Revision: 1.2 $"
 
 /*
  * Components of space reservations.
  */
+#define XFS_MAX_CONTIG_EXTENTS_PER_BLOCK(mp)    \
+                (((mp)->m_alloc_mxr[0]) - ((mp)->m_alloc_mnr[0]))
 #define	XFS_EXTENTADD_SPACE_RES(mp,w)	(XFS_BM_MAXLEVELS(mp,w) - 1)
+#define XFS_NEXTENTADD_SPACE_RES(mp,b,w)\
+        (((b + XFS_MAX_CONTIG_EXTENTS_PER_BLOCK(mp) - 1) / \
+          XFS_MAX_CONTIG_EXTENTS_PER_BLOCK(mp)) * \
+          XFS_EXTENTADD_SPACE_RES(mp,w))
 #define	XFS_DAENTER_SPACE_RES(mp,w)	\
 	((XFS_DA_NODE_MAXDEPTH + 1) * XFS_EXTENTADD_SPACE_RES(mp, w))
 #define	XFS_DAREMOVE_SPACE_RES(mp,w)	\
@@ -29,6 +35,7 @@
 	(1)
 #define	XFS_ATTRRM_SPACE_RES(mp)	\
 	XFS_DAREMOVE_SPACE_RES(mp, XFS_ATTR_FORK)
+/* This macro is not used - see inline code in xfs_attr_set */
 #define	XFS_ATTRSET_SPACE_RES(mp, v)	\
 	(XFS_DAENTER_SPACE_RES(mp, XFS_ATTR_FORK) + XFS_B_TO_FSB(mp, v))
 #define	XFS_CREATE_SPACE_RES(mp,n)	\
