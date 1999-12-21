f03b68af88254173fe78d973876636be26950659
Ken McDonell <kenmcd@sgi.com>
1999-12-21 10:01:54 +0000

    Encumbrance review done.  Add copyright and license words consistent with GPL.

--- fs/xfs/xfs_vfsops.c
+++ fs/xfs/xfs_vfsops.c
@@ -1,22 +1,28 @@
 /*
  * XFS filesystem operations.
  *
- * Copyright 1994, Silicon Graphics, Inc.
- * All Rights Reserved.
- *
- * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Silicon Graphics, Inc.;
- * the contents of this file may not be disclosed to third parties, copied or
- * duplicated in any form, in whole or in part, without the prior written
- * permission of Silicon Graphics, Inc.
- *
- * RESTRICTED RIGHTS LEGEND:
- * Use, duplication or disclosure by the Government is subject to restrictions
- * as set forth in subdivision (c)(1)(ii) of the Rights in Technical Data
- * and Computer Software clause at DFARS 252.227-7013, and/or in similar or
- * successor clauses in the FAR, DOD or NASA FAR Supplement. Unpublished -
- * rights reserved under the Copyright Laws of the United States.
+ * Copyright (C) 1999 Silicon Graphics, Inc.  All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Fondation.
+ * 
+ * This program is distributed in the hope that it would be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  Further, any license provided herein,
+ * whether implied or otherwise, is limited to this program in accordance with
+ * the express provisions of the GNU General Public License.  Patent licenses,
+ * if any, provided herein do not apply to combinations of this program with
+ * other product or programs, or any other product whatsoever.  This program is
+ * distributed without any warranty that the program is delivered free of the
+ * rightful claim of any third person by way of infringement or the like.  See
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston MA 02111-1307, USA.
  */
-#ident  "$Revision: 1.239 $"
+#ident  "$Revision: 1.240 $"
 
 #if defined(__linux__)
 #include <xfs_linux.h>
