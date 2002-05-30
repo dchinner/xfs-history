360ad1accce50339b49e22363bb21444273c5bb3
Steve Lord <lord@sgi.com>
2002-05-30 17:05:48 +0000

    Missing copyright

--- fs/xfs/pagebuf/page_buf_locking.c
+++ fs/xfs/pagebuf/page_buf_locking.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2002 Silicon Graphics, Inc.  All Rights Reserved.
+ * Portions Copyright (c) 2002 Christoph Hellwig.  All Rights Reserved.
  * 
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
