db3d6728b4ec0f104691d367df8bca789c616372
Nathan Scott <nathans@sgi.com>
2002-07-08 07:24:15 +0000

    no longer try to keep IRIX and Linux capabilities in sync, decided they have irreconcilable differences and that it isn't necessary anyway.

--- fs/xfs/xfs_cap.c
+++ fs/xfs/xfs_cap.c
@@ -32,12 +32,7 @@
 
 #include <xfs.h>
 
-#define XATTR_CAP_BITS	(sizeof(posix_cap_xattr_value) * NBBY)
-#define XFS_CAP_BITS	(sizeof(xfs_cap_value_t) * NBBY)
-
 STATIC int xfs_cap_allow_set(vnode_t *);
-STATIC int inline xfs_cap_lookup_xfs(uint);
-STATIC int inline xfs_cap_lookup_xattr(uint);
 
 
 /*
@@ -51,7 +46,7 @@ xfs_cap_vhascap(
 	int		len = sizeof(xfs_cap_set_t);
 	int		flags = ATTR_KERNOVAL|ATTR_ROOT;
 
-	VOP_ATTR_GET(vp, SGI_CAP_FILE, NULL, &len, flags, sys_cred, error);
+	VOP_ATTR_GET(vp, SGI_CAP_LINUX, NULL, &len, flags, sys_cred, error);
 	return (error == 0);
 }
 
@@ -64,45 +59,23 @@ posix_cap_xattr_to_xfs(
 	size_t			size,
 	xfs_cap_set_t		*dest)
 {
-	posix_cap_xattr_value	*src_value;
-	xfs_cap_value_t		*dest_value;
-	size_t			map_size;
-	int			n;
-
 	if (!src || !dest)
+		return EINVAL;
+
+	if (src->c_version != cpu_to_le32(POSIX_CAP_XATTR_VERSION))
+		return EINVAL;
+	if (src->c_abiversion != cpu_to_le32(_LINUX_CAPABILITY_VERSION))
 		return EINVAL;
 
 	if (size < sizeof(posix_cap_xattr))
 		return EINVAL;
 
-	if (src->c_version != cpu_to_le32(POSIX_CAP_XATTR_VERSION))
-		return EINVAL;
+	ASSERT(sizeof(dest->cap_effective) == sizeof(src->c_effective));
 
-	memset(dest, 0, sizeof(xfs_cap_set_t));
-	src_value = &src->c_effective;
-	dest_value = (xfs_cap_value_t *)dest;
-	map_size = XATTR_CAP_BITS >> BIT_TO_WORD_SHIFT;
+	dest->cap_effective	= src->c_effective;
+	dest->cap_permitted	= src->c_permitted;
+	dest->cap_inheritable	= src->c_inheritable;
 
-	/*
-	 * Iterate through the capability bits, setting corresponding XFS bits
-	 */
-	for (n = 0; n < 3; n++, src_value++, dest_value++) {
-		uint	setbit = xfs_next_bit(src_value, map_size, 0);
-
-		while (setbit != -1) {
-			int	bit = xfs_cap_lookup_xattr(setbit);
-
-			if (bit == -1) {
-#ifdef DEBUG
-				printk("XFS cannot map VFS capability bit %u\n",
-					setbit);
-#endif
-				return EINVAL;
-			}
-			*dest_value |= (1 << bit);
-			setbit = xfs_next_bit(src_value, map_size, setbit + 1);
-		}
-	}
 	return 0;
 }
 
@@ -115,42 +88,19 @@ posix_cap_xfs_to_xattr(
 	posix_cap_xattr		*xattr_cap,
 	size_t			size)
 {
-	posix_cap_xattr_value	*dest_value;
-	xfs_cap_value_t		*src_value;
 	size_t			new_size = posix_cap_xattr_size();
-	size_t			map_size;
-	int			n;
 
 	if (size < new_size)
 		return -ERANGE;	
 
-	memset(xattr_cap, 0, sizeof(posix_cap_xattr));
-	xattr_cap->c_version = cpu_to_le32(POSIX_CAP_XATTR_VERSION);
-	dest_value = (posix_cap_xattr_value *)&xattr_cap->c_effective;
-	src_value = (xfs_cap_value_t *)src;
-	map_size = XFS_CAP_BITS >> BIT_TO_WORD_SHIFT;
+	ASSERT(sizeof(xattr_cap->c_effective) == sizeof(src->cap_effective));
 
-	/*
-	 * Iterate through the XFS bits, setting corresponding capability bits
-	 */
-	for (n = 0; n < 3; n++, dest_value++, src_value++) {
-		uint	setbit = xfs_next_bit((uint *)src_value, map_size, 0);
+	xattr_cap->c_version	= cpu_to_le32(POSIX_CAP_XATTR_VERSION);
+	xattr_cap->c_abiversion	= cpu_to_le32(_LINUX_CAPABILITY_VERSION);
+	xattr_cap->c_effective	= src->cap_effective;
+	xattr_cap->c_permitted	= src->cap_permitted;
+	xattr_cap->c_inheritable= src->cap_inheritable;
 
-		while (setbit != -1) {
-			int	bit = xfs_cap_lookup_xfs(setbit);
-
-			if (bit == -1) {
-#ifdef DEBUG
-				printk("XFS cannot map XFS capability bit %u\n",
-					setbit);
-#endif
-				return -EINVAL;
-			}
-			*dest_value |= (1 << bit);
-			setbit = xfs_next_bit((uint *)src_value,
-						map_size, setbit + 1);
-		}
-	}
 	return new_size;
 }
 
@@ -172,7 +122,7 @@ xfs_cap_vget(
 
 	if (!size)
 		flags |= ATTR_KERNOVAL;
-	VOP_ATTR_GET(vp, SGI_CAP_FILE, (char *)&xfs_cap,
+	VOP_ATTR_GET(vp, SGI_CAP_LINUX, (char *)&xfs_cap,
 			&len, flags, sys_cred, error);
 	if (error)
 		goto out;
@@ -194,7 +144,7 @@ xfs_cap_vremove(
 	VN_HOLD(vp);
 	error = xfs_cap_allow_set(vp);
 	if (!error) {
-		VOP_ATTR_REMOVE(vp, SGI_CAP_FILE, ATTR_ROOT, sys_cred, error);
+		VOP_ATTR_REMOVE(vp, SGI_CAP_LINUX, ATTR_ROOT, sys_cred, error);
 		if (error == ENOATTR)
 			error = 0;	/* 'scool */
 	}
@@ -224,7 +174,7 @@ xfs_cap_vset(
 	if (error)
 		goto out;
 
-	VOP_ATTR_SET(vp, SGI_CAP_FILE, (char *)&xfs_cap,
+	VOP_ATTR_SET(vp, SGI_CAP_LINUX, (char *)&xfs_cap,
 			sizeof(xfs_cap_set_t), ATTR_ROOT, sys_cred, error);
 out:
 	VN_RELE(vp);
@@ -249,132 +199,4 @@ xfs_cap_allow_set(
 	if (va.va_uid != current->fsuid && !capable(CAP_FOWNER))
 		return EPERM;
 	return error;
-}
-
-
-/*
- * This translates between the XFS (ondisk) capability bits and the
- * corresponding VFS equivalents.
- * 
- * On IRIX, CAP_DAC_OVERRIDE is defined as:
- * 	(CAP_DAC_WRITE|CAP_DAC_READ_SEARCH|CAP_FOWNER)
- * But theres no CAP_DAC_WRITE under Linux and CAP_DAC_OVERRIDE is a
- * distinct bit.  So, we'll just define a XFS_CAP_LINUX_DAC_OVERRIDE
- * but we may need to revisit this I guess.
- */
-STATIC const char xfs_to_xattr_capbits[] = {
-	-1,			/* Currently unused		0 */
-	CAP_CHOWN,		/* XFS_CAP_CHOWN		1 */
-	-1,			/* XFS_CAP_DAC_WRITE		2 */
-	CAP_DAC_READ_SEARCH,	/* XFS_CAP_DAC_READ_SEARCH	3 */
-	CAP_FOWNER,		/* XFS_CAP_FOWNER		4 */
-	CAP_FSETID,		/* XFS_CAP_FSETID		5 */
-	CAP_KILL,		/* XFS_CAP_KILL			6 */
-	-1,			/* Currently unused		7 */
-	-1,			/* XFS_CAP_SETFPRIV		8 */
-	CAP_SETPCAP,		/* XFS_CAP_SETPPRIV		9 */
-	CAP_SETGID,		/* XFS_CAP_SETGID		10 */
-	CAP_SETUID,		/* XFS_CAP_SETUID		11 */
-	-1,			/* XFS_CAP_MAC_DOWNGRADE	12 */
-	-1,			/* XFS_CAP_MAC_READ		13 */
-	-1,			/* XFS_CAP_MAC_RELABEL_SUBJ	14 */
-	-1,			/* XFS_CAP_MAC_WRITE		15 */
-	-1,			/* XFS_CAP_MAC_UPGRADE		16 */
-	-1,			/* Currently unused		17 */
-	-1,			/* Currently unused		18 */
-	-1,			/* Currently unused		19 */
-	-1,			/* Currently unused		20 */
-	-1,			/* Currently unused		21 */
-	-1,			/* XFS_CAP_AUDIT_CONTROL	22 */
-	-1,			/* XFS_CAP_AUDIT_WRITE		23 */
-	-1,			/* XFS_CAP_MAC_MLD		24 */
-	-1,			/* XFS_CAP_MEMORY_MGT		25 */
-	-1,			/* XFS_CAP_SWAP_MGT		26 */
-	CAP_SYS_TIME,		/* XFS_CAP_TIME_MGT		27 */
-	-1,			/* XFS_CAP_SYSINFO_MGT		28 */
-	-1,			/* XFS_CAP_MOUNT_MGT		29 */
-	-1,			/* XFS_CAP_QUOTA_MGT		30 */
-	-1,			/* XFS_CAP_PRIV_PORT		31 */
-	-1,			/* XFS_CAP_STREAMS_MGT		32 */
-	CAP_SYS_NICE,		/* XFS_CAP_SCHED_MGT		33 */
-	-1,			/* XFS_CAP_PROC_MGT		34 */
-	-1,			/* XFS_CAP_SVIPC_MGT		35 */
-	CAP_NET_ADMIN,		/* XFS_CAP_NETWORK_MGT		36 */
-	CAP_MKNOD,		/* XFS_CAP_DEVICE_MGT		37 */
-	CAP_SYS_PACCT,		/* XFS_CAP_ACCT_MGT		38 */
-	CAP_SYS_BOOT,		/* XFS_CAP_SHUTDOWN		39 */
-	CAP_SYS_CHROOT,		/* XFS_CAP_CHROOT		40 */
-	-1,			/* XFS_CAP_DAC_EXECUTE		41 */
-	-1,			/* XFS_CAP_MAC_RELABEL_OPEN	42 */
-	-1,			/* Currently unused		43 */
-	-1,			/* XFS_CAP_XTCB			44 */
-	CAP_DAC_OVERRIDE,	/* XFS_CAP_LINUX_DAC_OVERRIDE	45 */
-	CAP_LINUX_IMMUTABLE,	/* XFS_CAP_LINUX_IMMUTABLE	46 */
-	CAP_NET_BIND_SERVICE,	/* XFS_CAP_LINUX_NET_BIND_SERVICE 47 */
-	CAP_NET_BROADCAST,	/* XFS_CAP_LINUX_NET_BROADCAST	48 */
-	CAP_NET_RAW,		/* XFS_CAP_LINUX_NET_RAW	49 */
-	CAP_IPC_LOCK,		/* XFS_CAP_LINUX_IPC_LOCK	50 */
-	CAP_IPC_OWNER,		/* XFS_CAP_LINUX_IPC_OWNER	51 */
-	CAP_SYS_MODULE,		/* XFS_CAP_LINUX_SYS_MODULE	52 */
-	CAP_SYS_RAWIO,		/* XFS_CAP_LINUX_SYS_RAWIO	53 */
-	CAP_SYS_PTRACE,		/* XFS_CAP_LINUX_SYS_PTRACE	54 */
-	CAP_SYS_ADMIN,		/* XFS_CAP_LINUX_SYS_ADMIN	55 */
-	CAP_SYS_RESOURCE,	/* XFS_CAP_LINUX_SYS_RESOURCE	56 */
-	CAP_SYS_TTY_CONFIG,	/* XFS_CAP_LINUX_SYS_TTY_CONFIG	57 */
-	CAP_LEASE,		/* XFS_CAP_LINUX_LEASE		58 */
-				/* Currently unused	59 ---> 63 */
-};
-
-STATIC int inline
-xfs_cap_lookup_xfs(uint bit)
-{
-	if (bit > sizeof(xfs_to_xattr_capbits)/sizeof(char))
-		return -1;
-	return xfs_to_xattr_capbits[bit];
-}
-
-
-/*
- * This translates between the VFS capability bits and their
- * corresponding XFS (ondisk) equivalents.
- */
-STATIC const char xattr_to_xfs_capbits[] = {
-	XFS_CAP_CHOWN,			/* CAP_CHOWN            0 */
-	XFS_CAP_LINUX_DAC_OVERRIDE,	/* CAP_DAC_OVERRIDE     1 */
-	XFS_CAP_DAC_READ_SEARCH,	/* CAP_DAC_READ_SEARCH  2 */
-	XFS_CAP_FOWNER,			/* CAP_FOWNER           3 */
-	XFS_CAP_FSETID,			/* CAP_FSETID           4 */
-	XFS_CAP_KILL,			/* CAP_KILL             5 */
-	XFS_CAP_SETGID,			/* CAP_SETGID           6 */
-	XFS_CAP_SETUID,			/* CAP_SETUID           7 */
-	XFS_CAP_SETPPRIV,		/* CAP_SETPCAP          8 */
-	XFS_CAP_LINUX_IMMUTABLE,	/* CAP_LINUX_IMMUTABLE  9 */
-	XFS_CAP_LINUX_NET_BIND_SERVICE,	/* CAP_NET_BIND_SERVICE 10 */
-	XFS_CAP_LINUX_NET_BROADCAST,	/* CAP_NET_BROADCAST    11 */
-	XFS_CAP_NETWORK_MGT,		/* CAP_NET_ADMIN        12 */
-	XFS_CAP_LINUX_NET_RAW,		/* CAP_NET_RAW          13 */
-	XFS_CAP_LINUX_IPC_LOCK,		/* CAP_IPC_LOCK         14 */
-	XFS_CAP_LINUX_IPC_OWNER,	/* CAP_IPC_OWNER        15 */
-	XFS_CAP_LINUX_SYS_MODULE,	/* CAP_SYS_MODULE       16 */
-	XFS_CAP_LINUX_SYS_RAWIO,	/* CAP_SYS_RAWIO        17 */
-	XFS_CAP_CHROOT,			/* CAP_SYS_CHROOT       18 */
-	XFS_CAP_LINUX_SYS_PTRACE,	/* CAP_SYS_PTRACE       19 */
-	XFS_CAP_ACCT_MGT,		/* CAP_SYS_PACCT        20 */
-	XFS_CAP_LINUX_SYS_ADMIN,	/* CAP_SYS_ADMIN        21 */
-	XFS_CAP_SHUTDOWN,		/* CAP_SYS_BOOT         22 */
-	XFS_CAP_SCHED_MGT,		/* CAP_SYS_NICE         23 */
-	XFS_CAP_LINUX_SYS_RESOURCE,	/* CAP_SYS_RESOURCE     24 */
-	XFS_CAP_TIME_MGT,		/* CAP_SYS_TIME         25 */
-	XFS_CAP_LINUX_SYS_TTY_CONFIG,	/* CAP_SYS_TTY_CONFIG   26 */
-	XFS_CAP_DEVICE_MGT,		/* CAP_MKNOD            27 */
-	XFS_CAP_LINUX_LEASE,		/* CAP_LEASE            28 */
-					/* Currently unused 29->31 */
-};
-
-STATIC int inline
-xfs_cap_lookup_xattr(uint bit)
-{
-	if (bit > sizeof(xattr_to_xfs_capbits)/sizeof(char))
-		return -1;
-	return xattr_to_xfs_capbits[bit];
 }
