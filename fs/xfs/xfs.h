fb11843cb118db27cb191934ae6d56a79816a924
Dean Roehrich <roehrich@sgi.com>
2002-07-25 16:40:28 +0000

    From Chris Hellwig:
    This one removes make the XFS filesystem code independent of the dmapi
    headers (include/linux/dmapi.h & include/linux/dmapi_kern.h).
    
    Details:
    * include <linux/dmapi.h> and <linux/dmapi_kern.h> in xfs_dmapi.h if
      CONFIG_XFS_DMAPI is set instead of unconditionally in xfs.h
    * make most of the current contents of xfs_dmapi.h depent on
      CONFIG_XFS_DMAPI
    * provide stubs for the dmapi functionality in xfs_dmapi.h otherwise
    * kill xfs_dmistubs.c
    No Message Supplied

--- fs/xfs/xfs.h
+++ fs/xfs/xfs.h
@@ -108,9 +108,6 @@
 #include <xfs_trans_priv.h>
 #include <xfs_trans_space.h>
 #include <xfs_utils.h>
-
-#include <linux/dmapi.h>
-#include <linux/dmapi_kern.h>
 #include <xfs_dmapi.h>
 
 #include <linux/xfs_lrw.h>
