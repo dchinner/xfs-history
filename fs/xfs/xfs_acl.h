57fcb3249421214d834e49c24696f656238892fe
Eric Sandeen <sandeen@sgi.com>
2002-07-31 22:09:41 +0000

    get rid of EOPNOTSUPP

--- fs/xfs/xfs_acl.h
+++ fs/xfs/xfs_acl.h
@@ -101,9 +101,9 @@ extern struct kmem_zone *xfs_acl_zone;
 #define _ACL_CLEAR_IFLAG(inode) ((inode)->i_flags &= ~S_POSIXACL)
 
 #else
-#define xfs_acl_vset(v,p,sz,t)	(-ENOTSUP)
-#define xfs_acl_vget(v,p,sz,t)	(-ENOTSUP)
-#define xfs_acl_vremove(v,t)	(-ENOTSUP)
+#define xfs_acl_vset(v,p,sz,t)	(-EOPNOTSUPP)
+#define xfs_acl_vget(v,p,sz,t)	(-EOPNOTSUPP)
+#define xfs_acl_vremove(v,t)	(-EOPNOTSUPP)
 #define _ACL_DECL(a)		((void)0)
 #define _ACL_ALLOC(a)		(1)	/* successfully allocate nothing */
 #define _ACL_FREE(a)		((void)0)
