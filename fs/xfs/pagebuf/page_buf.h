7582f3c9314af050307ffc6f558cd259b73b52cd
Christoph Hellwig <hch@sgi.com>
2002-10-01 15:00:51 +0000

    Handle NULL pagebufs gracefully in pagebuf_geterror Return ENOMEM for NULL pagebufs in pagebuf_geterror.

--- fs/xfs/pagebuf/page_buf.h
+++ fs/xfs/pagebuf/page_buf.h
@@ -305,8 +305,10 @@ extern void pagebuf_unlock(		/* unlock buffer		*/
 		page_buf_t *);		/* buffer to unlock		*/
 
 /* Buffer Utility Routines */
-
-#define pagebuf_geterror(pb)	((pb)->pb_error)
+static inline int pagebuf_geterror(page_buf_t *pb)
+{
+	return (pb ? pb->pb_error : ENOMEM);
+}
 
 extern void pagebuf_queue_task(
 		struct tq_struct *);
