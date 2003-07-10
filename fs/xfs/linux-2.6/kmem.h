c1f61421e02cbab155fc3e4efce98fe81dfb4d75
Nathan Scott <nathans@sgi.com>
2003-07-10 05:51:44 +0000

    Undoes mod:     2.5.x-xfs:slinx:152848a Missed date-in-comment update from last merge

--- fs/xfs/linux-2.6/kmem.h
+++ fs/xfs/linux-2.6/kmem.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000-2002 Silicon Graphics, Inc.  All Rights Reserved.
+ * Copyright (c) 2000-2003 Silicon Graphics, Inc.  All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -86,7 +86,7 @@ kmem_flags_convert(int flags)
 	lflags = (flags & KM_NOSLEEP) ? GFP_ATOMIC : (GFP_KERNEL|__GFP_NOFAIL);
 
 	/* avoid recusive callbacks to filesystem during transactions */
-	if (PFLAGS_TEST_FSTRANS() || (flags & KM_NOFS))
+	if (PFLAGS_TEST_FSTRANS())
 		lflags &= ~__GFP_FS;
 
 	return lflags;
