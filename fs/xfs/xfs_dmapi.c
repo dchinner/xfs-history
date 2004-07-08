e34837dd2e6b8cbce6cb641ede6efc209e513887
Nathan Scott <nathans@sgi.com>
2004-07-08 07:35:45 +0000

    sparse: rework previous mods to fix warnings in DMAPI code.  From Chris Wedgwood.

--- fs/xfs/xfs_dmapi.c
+++ fs/xfs/xfs_dmapi.c
@@ -478,7 +478,7 @@ STATIC int
 xfs_dm_bulkstat_one(
 	xfs_mount_t	*mp,		/* mount point for filesystem */
 	xfs_ino_t	ino,		/* inode number to get data for */
-	void		*buffer,	/* buffer to place output in */
+	void __user	*buffer,	/* buffer to place output in */
 	int		ubsize,		/* size of buffer */
 	void		*private_data,	/* my private data */
 	xfs_daddr_t	bno,		/* starting block of inode cluster */
