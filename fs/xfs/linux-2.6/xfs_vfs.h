1476ae8c98a82400918820f795ee3284b7872fee
Christoph Hellwig <hch@sgi.com>
2005-06-08 10:10:57 +0000

    quiesce the filesystem proper when freezing add SYNC_QUIESCE flag

--- fs/xfs/linux-2.6/xfs_vfs.h
+++ fs/xfs/linux-2.6/xfs_vfs.h
@@ -107,6 +107,7 @@ typedef enum {
 #define SYNC_FSDATA		0x0020	/* flush fs data (e.g. superblocks) */
 #define SYNC_REFCACHE		0x0040  /* prune some of the nfs ref cache */
 #define SYNC_REMOUNT		0x0080  /* remount readonly, no dummy LRs */
+#define SYNC_QUIESCE		0x0100  /* quiesce fileystem for a snapshot */
 
 #define IGET_NOALLOC		0x0001	/* vfs_get_inode may return NULL */
 
