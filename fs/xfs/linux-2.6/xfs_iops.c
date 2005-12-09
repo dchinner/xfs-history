96a63f80f815682ed7db7dec6f20ecc9de775852
Nathan Scott <nathans@sgi.com>
2005-12-09 05:04:28 +0000

    Add an XFS callout to security_inode_init_security; SE Linux is not functional with XFS without this change. Merge of xfs-linux-melb:xfs-kern:24766a by kenmcd.

--- fs/xfs/linux-2.6/xfs_iops.c
+++ fs/xfs/linux-2.6/xfs_iops.c
@@ -53,6 +53,7 @@
 
 #include <linux/xattr.h>
 #include <linux/namei.h>
+#include <linux/security.h>
 
 /*
  * Bring the atime in the XFS inode uptodate.
@@ -200,6 +201,39 @@ validate_fields(
 }
 
 /*
+ * Hook in SELinux.  This is not quite correct yet, what we really need
+ * here (as we do for default ACLs) is a mechanism by which creation of
+ * these attrs can be journalled at inode creation time (along with the
+ * inode, of course, such that log replay can't cause these to be lost).
+ */
+STATIC int
+linvfs_init_security(
+	struct vnode	*vp,
+	struct inode	*dir)
+{
+	struct inode	*ip = LINVFS_GET_IP(vp);
+	size_t		length;
+	void		*value;
+	char		*name;
+	int		error;
+
+	error = security_inode_init_security(ip, dir, &name, &value, &length);
+	if (error) {
+		if (error == -EOPNOTSUPP)
+			return 0;
+		return -error;
+	}
+
+	VOP_ATTR_SET(vp, name, value, length, ATTR_SECURE, NULL, error);
+	if (!error)
+		VMODIFY(vp);
+
+	kfree(name);
+	kfree(value);
+	return error;
+}
+
+/*
  * Determine whether a process has a valid fs_struct (kernel daemons
  * like knfsd don't have an fs_struct).
  *
@@ -263,6 +297,9 @@ linvfs_mknod(
 		error = EINVAL;
 		break;
 	}
+
+	if (!error)
+		error = linvfs_init_security(vp, dir);
 
 	if (default_acl) {
 		if (!error) {
