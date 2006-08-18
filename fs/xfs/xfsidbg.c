8aaba36c9e63d0ca852a8cadb1a0ff9e876410f2
Tim Shimmin <tes@sgi.com>
2006-08-18 15:34:19 +0000

    Fixes the leak in reservation space because we weren't ungranting space for the unmount record - which becomes a problem in the freeze/thaw scenario. Merge of xfs-linux-melb:xfs-kern:26815a by kenmcd.
      Add some missing transaction types to be printed.

--- fs/xfs/xfsidbg.c
+++ fs/xfs/xfsidbg.c
@@ -6260,6 +6260,10 @@ xfsidbg_print_trans_type(unsigned int t_type)
 	case XFS_TRANS_GROWFSRT_ALLOC:	kdb_printf("GROWFSRT_ALLOC");	break;
 	case XFS_TRANS_GROWFSRT_ZERO:	kdb_printf("GROWFSRT_ZERO");	break;
 	case XFS_TRANS_GROWFSRT_FREE:	kdb_printf("GROWFSRT_FREE");	break;
+  	case XFS_TRANS_SWAPEXT:		kdb_printf("SWAPEXT");		break;
+ 	case XFS_TRANS_DUMMY1:		kdb_printf("DUMMY1");		break;
+ 	case XFS_TRANS_DUMMY2:		kdb_printf("DUMMY2");		break;
+ 	case XLOG_UNMOUNT_REC_TYPE:	kdb_printf("UNMOUNT");		break;
 	default:			kdb_printf("unknown(0x%x)", t_type); break;
 	}
 }
